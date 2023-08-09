3f58e8d08ad27b25ede5feb73c5c97e4ea4cc947
Rohitm19 <mudaliar.extc.engg@gmail.com>
2023-08-09 00:36:30 -0400

    validAnagram (vsCode) - comments for explaination

--- Arrays & Hashing/2.validAnagram.py
+++ Arrays & Hashing/2.validAnagram.py
@@ -6,13 +6,17 @@ class Solution:                                       #main logic
         if len(s) != len(t):
             return False
 
-        countS, countT = {}, {}
+        countS, countT = {}, {}                      # creating hashmap
 
         for i in range(len(s)):
-            countS[s[i]] = 1 + countS.get(s[i], 0)
+            print(s)
+            countS[s[i]] = 1 + countS.get(s[i], 0)     # we can access the value of a dictionary with the syntax dictionary[key]. In this case, the key is the character s[i]. So, the line countS[s[i]] is accessing the value of the dictionary countS for the key s[i]. 
+            print(countS)                              # so  countS[s[i]] = r for first iteration
+                                                       # &  1 + countS.get(s[i], 0) = 1 . This tries to get the s[i] from the dictonary countS. 
             countT[t[i]] = 1 + countT.get(t[i], 0)
-        return countS == countT
-
+        
+        return countS == countT                        #The return countS == countT line returns True if the two dictionaries countS and countT are equal. Otherwise, the function returns False
+ 
 def main():                                            # logic for manual input
     s = input("Enter the first string: ")
     t = input("Enter the second string: ")
