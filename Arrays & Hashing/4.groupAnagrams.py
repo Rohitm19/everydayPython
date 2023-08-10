c8cb75f3bc4d84193e473c02ea45b5f5a49af770
Rohitm19 <mudaliar.extc.engg@gmail.com>
2023-08-10 18:10:15 -0400

    groupAnagrams (vsCode)

--- Arrays & Hashing/4.groupAnagrams.py
+++ Arrays & Hashing/4.groupAnagrams.py
@@ -1,7 +1,10 @@
-#neetCode
+#VS Code
+from typing import List
+from collections import defaultdict
+
 class Solution:
     def groupAnagrams(self, strs: List[str]) -> List[List[str]]:
-        ans = collections.defaultdict(list)
+        ans = defaultdict(list)
 
         for s in strs:
             count = [0] * 26
@@ -9,3 +12,20 @@ class Solution:
                 count[ord(c) - ord("a")] += 1
             ans[tuple(count)].append(s)
         return ans.values()
+
+def main():
+    strs = []
+    while True:
+        s = input("Enter a string: ")
+        if s == "":
+            break
+        strs.append(s)
+
+    solution = Solution()
+    result = solution.groupAnagrams(strs)
+
+    for group in result:
+        print(group)
+
+if __name__ == "__main__":
+    main()
