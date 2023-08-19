a66397cb018e8beff49cd1163f9d2b8de16a41b1
Rohitm19 <mudaliar.extc.engg@gmail.com>
2023-08-18 20:51:07 -0400

    reverseLinkedList (vsCode)

--- 6. Linked List/1.reverseLinkedList.py
+++ 6. Linked List/1.reverseLinkedList.py
@@ -1,11 +1,7 @@
-#neetCode
-
-# Definition for singly-linked list.
-# class ListNode:
-#     def __init__(self, x):
-#         self.val = x
-#         self.next = None
-
+class ListNode:
+    def __init__(self, val=0, next=None):
+        self.val = val
+        self.next = next
 
 class Solution:
     def reverseList(self, head: ListNode) -> ListNode:
@@ -17,3 +13,35 @@ class Solution:
             prev = curr
             curr = temp
         return prev
+
+def create_linked_list(nums):
+    dummy = ListNode()
+    current = dummy
+    for num in nums:
+        current.next = ListNode(num)
+        current = current.next
+    return dummy.next
+
+def print_linked_list(head):
+    current = head
+    while current:
+        print(current.val, end=" -> ")
+        current = current.next
+    print("None")
+
+def main():
+    nums = input("Enter a list of numbers separated by spaces: ").split()
+    nums = [int(num) for num in nums]
+    head = create_linked_list(nums)
+    
+    solution = Solution()
+    reversed_head = solution.reverseList(head)
+    
+    print("Original Linked List:")
+    print_linked_list(head)
+    
+    print("Reversed Linked List:")
+    print_linked_list(reversed_head)
+
+if __name__ == "__main__":
+    main()
