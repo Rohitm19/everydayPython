248eb177ed0d92836d45b57179be39b382232c2b
Rohitm19 <mudaliar.extc.engg@gmail.com>
2023-08-18 20:45:30 -0400

    searchInRotatedSortedArray (vsCode)

--- 5. Binary Search/2.searchInRotatedSortedArray.py
+++ 5. Binary Search/2.searchInRotatedSortedArray.py
@@ -1,4 +1,4 @@
-#neetCode
+#vsCode
 
 class Solution:
     def search(self, nums: List[int], target: int) -> int:
@@ -22,3 +22,19 @@ class Solution:
                 else:
                     l = mid + 1
         return -1
+
+def main():
+    nums = input("Enter a list of numbers separated by spaces: ").split()
+    nums = [int(num) for num in nums]
+    target = int(input("Enter the target number: "))
+    
+    solution = Solution()
+    index = solution.search(nums, target)
+    
+    if index != -1:
+        print("Target number found at index:", index)
+    else:
+        print("Target number not found in the list.")
+
+if __name__ == "__main__":
+    main()
