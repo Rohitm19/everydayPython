3179d2c1e44766fc684247ac52503a52298b8297
Rohitm19 <mudaliar.extc.engg@gmail.com>
2023-08-25 13:08:09 -0400

    binaryTreeLevelOrderTraversal (vsCode)

--- 7. Trees/6.binaryTreeLevelOrderTraversal.py
+++ 7. Trees/6.binaryTreeLevelOrderTraversal.py
@@ -1,17 +1,17 @@
-#neetcode
+#vsCode
 
-# Definition for a binary tree node.
-# class TreeNode:
-#     def __init__(self, x):
-#         self.val = x
-#         self.left = None
-#         self.right = None
+from collections import deque
 
+class TreeNode:
+    def __init__(self, x):
+        self.val = x
+        self.left = None
+        self.right = None
 
 class Solution:
     def levelOrder(self, root: TreeNode) -> List[List[int]]:
         res = []
-        q = collections.deque()
+        q = deque()
         if root:
             q.append(root)
 
@@ -27,3 +27,28 @@ class Solution:
                     q.append(node.right)
             res.append(val)
         return res
+
+def build_tree(nodes, index):
+    if index >= len(nodes) or nodes[index] is None:
+        return None
+
+    node = TreeNode(nodes[index])
+    node.left = build_tree(nodes, 2 * index + 1)
+    node.right = build_tree(nodes, 2 * index + 2)
+    return node
+
+def main():
+    nodes = input("Enter a list of node values separated by spaces (use 'None' for missing nodes): ").split()
+    nodes = [int(node) if node != "None" else None for node in nodes]
+    
+    root = build_tree(nodes, 0)
+    solution = Solution()
+    
+    level_order_result = solution.levelOrder(root)
+    
+    print("Level-Order Traversal Result:")
+    for level in level_order_result:
+        print(level)
+
+if __name__ == "__main__":
+    main()
