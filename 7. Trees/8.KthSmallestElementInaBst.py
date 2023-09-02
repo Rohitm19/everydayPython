d57a72553ea50c40082439274e7c00c0cbc75dee
Rohitm19 <mudaliar.extc.engg@gmail.com>
2023-09-02 16:39:17 -0400

    KthSmallestElementInaBst (vsCode)

--- 7. Trees/8.KthSmallestElementInaBst.py
+++ 7. Trees/8.KthSmallestElementInaBst.py
@@ -1,12 +1,10 @@
-#neetcode
+#vsCode
 
-# Definition for a binary tree node.
-# class TreeNode:
-#     def __init__(self, x):
-#         self.val = x
-#         self.left = None
-#         self.right = None
-
+class TreeNode:
+    def __init__(self, val=0, left=None, right=None):
+        self.val = val
+        self.left = left
+        self.right = right
 
 class Solution:
     def kthSmallest(self, root: TreeNode, k: int) -> int:
@@ -22,3 +20,40 @@ class Solution:
             if k == 0:
                 return curr.val
             curr = curr.right
+
+def build_bst(nodes):
+    if not nodes:
+        return None
+
+    root = TreeNode(nodes[0])
+    for node_val in nodes[1:]:
+        insert_node(root, node_val)
+
+    return root
+
+def insert_node(root, val):
+    if not root:
+        return TreeNode(val)
+
+    if val < root.val:
+        root.left = insert_node(root.left, val)
+    else:
+        root.right = insert_node(root.right, val)
+
+    return root
+
+def main():
+    nodes = input("Enter a list of node values separated by spaces to create a Binary Search Tree (BST): ").split()
+    nodes = [int(node) for node in nodes]
+    
+    k = int(input("Enter the value of k to find the kth smallest element: "))
+    
+    root = build_bst(nodes)
+    solution = Solution()
+    
+    kth_smallest = solution.kthSmallest(root, k)
+    
+    print(f"The {k}th smallest element in the BST is:", kth_smallest)
+
+if __name__ == "__main__":
+    main()
