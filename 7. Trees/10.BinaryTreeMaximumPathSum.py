744b0039e2b54460dfc455b1d5d74b11ceb78f5a
Rohitm19 <mudaliar.extc.engg@gmail.com>
2023-09-03 14:32:01 -0400

    BinaryTreeMaximumPathSum (vsCode)

--- 7. Trees/10.BinaryTreeMaximumPathSum.py
+++ 7. Trees/10.BinaryTreeMaximumPathSum.py
@@ -1,11 +1,11 @@
-#neetCode
+#vsCode
 
-# Definition for a binary tree node.
-# class TreeNode:
-#     def __init__(self, val=0, left=None, right=None):
-#         self.val = val
-#         self.left = left
-#         self.right = right
+class TreeNode:
+    def __init__(self, val=0, left=None, right=None):
+        self.val = val
+        self.left = left
+        self.right = right
+
 class Solution:
     def maxPathSum(self, root: TreeNode) -> int:
         res = [root.val]
@@ -26,3 +26,26 @@ class Solution:
 
         dfs(root)
         return res[0]
+
+def build_tree(nodes, index):
+    if index >= len(nodes) or nodes[index] is None:
+        return None
+
+    node = TreeNode(nodes[index])
+    node.left = build_tree(nodes, 2 * index + 1)
+    node.right = build_tree(nodes, 2 * index + 2)
+    return node
+
+def main():
+    nodes = input("Enter a list of node values separated by spaces (use 'None' for missing nodes): ").split()
+    nodes = [int(node) if node != "None" else None for node in nodes]
+    
+    root = build_tree(nodes, 0)
+    solution = Solution()
+    
+    max_path_sum = solution.maxPathSum(root)
+    
+    print("Maximum Path Sum in the Binary Tree:", max_path_sum)
+
+if __name__ == "__main__":
+    main()
