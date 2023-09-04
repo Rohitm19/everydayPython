587bf78546e3d3abd0e06e116933c2a449902310
Rohitm19 <mudaliar.extc.engg@gmail.com>
2023-09-04 15:29:50 -0400

    ImplementTriePrefixTree (vsCode)

--- 8. Tries/1.ImplementTriePrefixTree.py
+++ 8. Tries/1.ImplementTriePrefixTree.py
@@ -1,4 +1,4 @@
-#neetcode
+#vsCode
 
 class TrieNode:
     def __init__(self):
@@ -20,7 +20,7 @@ class Trie:
         curr = self.root
         for c in word:
             i = ord(c) - ord("a")
-            if curr.children[i] == None:
+            if curr.children[i] is None:
                 curr.children[i] = TrieNode()
             curr = curr.children[i]
         curr.end = True
@@ -32,7 +32,7 @@ class Trie:
         curr = self.root
         for c in word:
             i = ord(c) - ord("a")
-            if curr.children[i] == None:
+            if curr.children[i] is None:
                 return False
             curr = curr.children[i]
         return curr.end
@@ -44,7 +44,43 @@ class Trie:
         curr = self.root
         for c in prefix:
             i = ord(c) - ord("a")
-            if curr.children[i] == None:
+            if curr.children[i] is None:
                 return False
             curr = curr.children[i]
         return True
+
+def main():
+    trie = Trie()
+    
+    while True:
+        print("Choose an operation:")
+        print("1. Insert a word")
+        print("2. Search for a word")
+        print("3. Check if a word starts with a prefix")
+        print("4. Exit")
+        
+        choice = int(input("Enter your choice: "))
+        
+        if choice == 1:
+            word = input("Enter the word to insert: ")
+            trie.insert(word)
+            print(f"'{word}' has been inserted into the Trie.")
+        elif choice == 2:
+            word = input("Enter the word to search for: ")
+            if trie.search(word):
+                print(f"'{word}' is in the Trie.")
+            else:
+                print(f"'{word}' is not in the Trie.")
+        elif choice == 3:
+            prefix = input("Enter the prefix to check: ")
+            if trie.startsWith(prefix):
+                print(f"There are words in the Trie that start with '{prefix}'.")
+            else:
+                print(f"No words in the Trie start with '{prefix}'.")
+        elif choice == 4:
+            break
+        else:
+            print("Invalid choice. Please choose a valid operation.")
+
+if __name__ == "__main__":
+    main()
