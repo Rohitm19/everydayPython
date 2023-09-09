b7e6544bbfb08b2faa64e58d04536a91f58162a0
Rohitm19 <mudaliar.extc.engg@gmail.com>
2023-09-09 18:05:23 -0400

    wordSearch (vsCode)

--- 10. Backtracking/2.wordSearch.py
+++ 10. Backtracking/2.wordSearch.py
@@ -1,7 +1,9 @@
-#neetCode
+#vsCode
+
+from collections import Counter, defaultdict
 
 class Solution:
-    def exist(self, board: List[List[str]], word: str) -> bool:
+    def exist(self, board, word):
         ROWS, COLS = len(board), len(board[0])
         path = set()
 
@@ -26,15 +28,32 @@ class Solution:
             path.remove((r, c))
             return res
 
-        # To prevent TLE,reverse the word if frequency of the first letter is more than the last letter's
+        # To prevent TLE, reverse the word if the frequency of the first letter is more than the last letter's
         count = defaultdict(int, sum(map(Counter, board), Counter()))
         if count[word[0]] > count[word[-1]]:
             word = word[::-1]
-            
+
+        board = []
+        for i in range(ROWS):
+            row = input(f"Enter row {i + 1} as a string of characters: ").split()
+            board.append(row)
+
+        word = input("Enter the word to search for: ")
+
         for r in range(ROWS):
             for c in range(COLS):
                 if dfs(r, c, 0):
                     return True
         return False
 
-    # O(n * m * 4^n)
+def main():
+    solution = Solution()
+    found = solution.exist([], "")
+    
+    if found:
+        print("The word exists on the board.")
+    else:
+        print("The word does not exist on the board.")
+
+if __name__ == "__main__":
+    main()
