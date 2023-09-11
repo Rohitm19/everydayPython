84e0d13bc587a60ba9510f0913aa925020c48819
Rohitm19 <mudaliar.extc.engg@gmail.com>
2023-09-11 19:13:21 -0400

    courseSchedule (vsCode)

--- 11. Graphs/4.courseSchedule.py
+++ 11. Graphs/4.courseSchedule.py
@@ -1,12 +1,15 @@
-#neetCode
+#vsCode
+
+from typing import List
 
 class Solution:
     def canFinish(self, numCourses: int, prerequisites: List[List[int]]) -> bool:
         # dfs
         preMap = {i: [] for i in range(numCourses)}
 
-        # map each course to : prereq list
-        for crs, pre in prerequisites:
+        # Manually input prerequisites
+        for i in range(len(prerequisites)):
+            crs, pre = map(int, input(f"Enter prerequisite for course {i + 1} (course prerequisite): ").split())
             preMap[crs].append(pre)
 
         visiting = set()
@@ -29,3 +32,21 @@ class Solution:
             if not dfs(c):
                 return False
         return True
+
+def main():
+    solution = Solution()
+    numCourses = int(input("Enter the number of courses: "))
+    prerequisites = []
+    num_prerequisites = int(input("Enter the number of prerequisites: "))
+    for i in range(num_prerequisites):
+        crs, pre = map(int, input(f"Enter prerequisite for course {i + 1} (course prerequisite): ").split())
+        prerequisites.append([crs, pre])
+
+    result = solution.canFinish(numCourses, prerequisites)
+    if result:
+        print("You can finish all courses.")
+    else:
+        print("You cannot finish all courses due to circular dependencies.")
+
+if __name__ == "__main__":
+    main()
