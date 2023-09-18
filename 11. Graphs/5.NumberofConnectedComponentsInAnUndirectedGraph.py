5e5e79804150e8ea2eb2e0855f4797bdd7b06df3
Rohitm19 <mudaliar.extc.engg@gmail.com>
2023-09-17 23:47:03 -0400

    NumberofConnectedComponentsInAnUndirectedGraph (vsCode)

--- 11. Graphs/5.NumberofConnectedComponentsInAnUndirectedGraph.py
+++ 11. Graphs/5.NumberofConnectedComponentsInAnUndirectedGraph.py
@@ -1,4 +1,4 @@
-#neetCode
+from typing import List
 
 class UnionFind:
     def __init__(self):
@@ -17,6 +17,26 @@ class UnionFind:
 class Solution:
     def countComponents(self, n: int, edges: List[List[int]]) -> int:
         dsu = UnionFind()
-        for a, b in edges:
+
+        # Manually input edges
+        for i in range(len(edges)):
+            a, b = map(int, input(f"Enter edge {i + 1} (node a, node b): ").split())
+            edges[i] = [a, b]
             dsu.union(a, b)
+
         return len(set(dsu.findParent(x) for x in range(n)))
+
+def main():
+    solution = Solution()
+    n = int(input("Enter the number of components: "))
+    num_edges = int(input("Enter the number of edges: "))
+    edges = []
+    for i in range(num_edges):
+        a, b = map(int, input(f"Enter edge {i + 1} (node a, node b): ").split())
+        edges.append([a, b])
+
+    result = solution.countComponents(n, edges)
+    print("Number of components:", result)
+
+if __name__ == "__main__":
+    main()
