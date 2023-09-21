ee40f9fc65f11fa6aa2cbda9cdab98c5f18dedfd
Rohitm19 <mudaliar.extc.engg@gmail.com>
2023-09-21 18:15:01 -0400

    coinChange (vsCode)

--- 13. 1-D Dynamic Programming/7.coinChange.py
+++ 13. 1-D Dynamic Programming/7.coinChange.py
@@ -1,4 +1,4 @@
-#neetCode
+#vsCode
 
 class Solution:
     def coinChange(self, coins: List[int], amount: int) -> int:
@@ -10,3 +10,14 @@ class Solution:
                 if a - c >= 0:
                     dp[a] = min(dp[a], 1 + dp[a - c])
         return dp[amount] if dp[amount] != amount + 1 else -1
+
+def main():
+    solution = Solution()
+    coins_input = input("Enter the coin denominations (comma-separated): ")
+    coins = [int(coin) for coin in coins_input.split(',')]
+    amount = int(input("Enter the amount: "))
+    result = solution.coinChange(coins, amount)
+    print("Minimum number of coins needed:", result)
+
+if __name__ == "__main__":
+    main()
