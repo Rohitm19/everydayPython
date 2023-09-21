1239dac1ee8a75606e8cbc024cab4cf7fbbaca18
Rohitm19 <mudaliar.extc.engg@gmail.com>
2023-09-21 18:12:22 -0400

    decodeWays (vsCode)

--- 13. 1-D Dynamic Programming/6.decodeWays.py
+++ 13. 1-D Dynamic Programming/6.decodeWays.py
@@ -1,26 +1,7 @@
-#neetcode
+#vsCode
 
 class Solution:
     def numDecodings(self, s: str) -> int:
-        # Memoization
-        dp = {len(s): 1}
-
-        def dfs(i):
-            if i in dp:
-                return dp[i]
-            if s[i] == "0":
-                return 0
-
-            res = dfs(i + 1)
-            if i + 1 < len(s) and (
-                s[i] == "1" or s[i] == "2" and s[i + 1] in "0123456"
-            ):
-                res += dfs(i + 2)
-            dp[i] = res
-            return res
-
-        return dfs(0)
-
         # Dynamic Programming
         dp = {len(s): 1}
         for i in range(len(s) - 1, -1, -1):
@@ -34,3 +15,12 @@ class Solution:
             ):
                 dp[i] += dp[i + 2]
         return dp[0]
+
+def main():
+    solution = Solution()
+    s = input("Enter the decoding string: ")
+    result = solution.numDecodings(s)
+    print("Number of possible decodings:", result)
+
+if __name__ == "__main__":
+    main()
