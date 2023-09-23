d4ba5baca33ed1e8d082362bb4bbc7cfed85a76d
Rohitm19 <mudaliar.extc.engg@gmail.com>
2023-09-23 19:15:10 -0400

    maximumProductSubarray (vsCode)

--- 13. 1-D Dynamic Programming/8.maximumProductSubarray.py
+++ 13. 1-D Dynamic Programming/8.maximumProductSubarray.py
@@ -1,4 +1,4 @@
-#neetCode
+#vsCode
 
 class Solution:
     def maxProduct(self, nums: List[int]) -> int:
@@ -7,9 +7,18 @@ class Solution:
         curMin, curMax = 1, 1
 
         for n in nums:
-
             tmp = curMax * n
             curMax = max(n * curMax, n * curMin, n)
             curMin = min(tmp, n * curMin, n)
             res = max(res, curMax)
         return res
+
+def main():
+    solution = Solution()
+    nums_input = input("Enter the list of integers (comma-separated): ")
+    nums = [int(num) for num in nums_input.split(',')]
+    result = solution.maxProduct(nums)
+    print("Maximum product of a subarray:", result)
+
+if __name__ == "__main__":
+    main()
