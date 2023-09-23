b9d5c757fed36cb6153a75d734775819a3c5e476
Rohitm19 <mudaliar.extc.engg@gmail.com>
2023-09-23 19:18:26 -0400

    wordBreak (vsCode)

--- 13. 1-D Dynamic Programming/9.wordBreak.py
+++ 13. 1-D Dynamic Programming/9.wordBreak.py
@@ -1,8 +1,7 @@
-#neetCode
+#vsCode
 
 class Solution:
     def wordBreak(self, s: str, wordDict: List[str]) -> bool:
-
         dp = [False] * (len(s) + 1)
         dp[len(s)] = True
 
@@ -14,3 +13,14 @@ class Solution:
                     break
 
         return dp[0]
+
+def main():
+    solution = Solution()
+    s = input("Enter the string: ")
+    word_dict_input = input("Enter the word dictionary (comma-separated): ")
+    word_dict = word_dict_input.split(',')
+    result = solution.wordBreak(s, word_dict)
+    print("Can be segmented into words from the word dictionary:", result)
+
+if __name__ == "__main__":
+    main()
