ab4f9a22a8002d52ce852f6e8f66a834bd1dd16d
Rohitm19 <mudaliar.extc.engg@gmail.com>
2023-09-28 17:57:13 -0400

    insertInterval ()

--- 16.Intervals/1.insertInterval.py
+++ 16.Intervals/1.insertInterval.py
@@ -1,9 +1,9 @@
-#neetCode
+#vsCode
+
+from typing import List
 
 class Solution:
-    def insert(
-        self, intervals: List[List[int]], newInterval: List[int]
-    ) -> List[List[int]]:
+    def insert(self, intervals: List[List[int]], newInterval: List[int]) -> List[List[int]]:
         res = []
 
         for i in range(len(intervals)):
@@ -19,3 +19,19 @@ class Solution:
                 ]
         res.append(newInterval)
         return res
+
+def main():
+    solution = Solution()
+    intervals = []
+    num_intervals = int(input("Enter the number of intervals: "))
+    for i in range(num_intervals):
+        interval = list(map(int, input(f"Enter interval {i + 1} as [start, end]: ").split(',')))
+        intervals.append(interval)
+
+    new_interval = list(map(int, input("Enter the new interval as [start, end]: ").split(',')))
+
+    result = solution.insert(intervals, new_interval)
+    print("Merged intervals:", result)
+
+if __name__ == "__main__":
+    main()
