5d848706169a63f3540de73a4c48bd040761a48e
Rohitm19 <mudaliar.extc.engg@gmail.com>
2023-09-27 22:35:45 -0400

    maxi

--- 15. Greedy/1.maximumSubarray.py
+++ 15. Greedy/1.maximumSubarray.py
@@ -1,4 +1,6 @@
-#neetCode
+#vsCode
+
+from typing import List
 
 class Solution:
     def maxSubArray(self, nums: List[int]) -> int:
@@ -11,3 +13,12 @@ class Solution:
             if total < 0:
                 total = 0
         return res
+
+def main():
+    solution = Solution()
+    nums = list(map(int, input("Enter the list of numbers, separated by spaces: ").split()))
+    result = solution.maxSubArray(nums)
+    print("Maximum Subarray Sum:", result)
+
+if __name__ == "__main__":
+    main()
