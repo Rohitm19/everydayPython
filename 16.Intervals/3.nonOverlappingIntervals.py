0a3fcf0bf7c6f96008e1803f34e0ef6ee0ee4e9f
Rohitm19 <mudaliar.extc.engg@gmail.com>
2023-09-29 23:15:20 -0400

    nonOverlappingIntervals (vsCode)

--- 16.Intervals/3.nonOverlappingIntervals.py
+++ 16.Intervals/3.nonOverlappingIntervals.py
@@ -1,4 +1,6 @@
-#neetCode
+#vsCode
+
+from typing import List
 
 class Solution:
     def eraseOverlapIntervals(self, intervals: List[List[int]]) -> int:
@@ -12,3 +14,17 @@ class Solution:
                 res += 1
                 prevEnd = min(end, prevEnd)
         return res
+
+def main():
+    solution = Solution()
+    intervals = []
+    num_intervals = int(input("Enter the number of intervals: "))
+    for i in range(num_intervals):
+        interval = list(map(int, input(f"Enter interval {i + 1} as [start, end]: ").split(',')))
+        intervals.append(interval)
+
+    result = solution.eraseOverlapIntervals(intervals)
+    print("Minimum number of intervals to remove:", result)
+
+if __name__ == "__main__":
+    main()
