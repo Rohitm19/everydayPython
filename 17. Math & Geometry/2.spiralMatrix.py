5199418efcd5295829e35a99705a2fd15e7ce256
Rohitm19 <mudaliar.extc.engg@gmail.com>
2023-10-03 23:53:27 -0400

    spiralMatrix (vsCode)

--- 17. Math & Geometry/2.spiralMatrix.py
+++ 17. Math & Geometry/2.spiralMatrix.py
@@ -1,29 +1,53 @@
-#neetCode
+#vsCode
+
+from typing import List
 
 class Solution:
     def spiralOrder(self, matrix: List[List[int]]) -> List[int]:
         res = []
-        left, right = 0, len(matrix[0])
-        top, bottom = 0, len(matrix)
+        if not matrix:
+            return res
 
-        while left < right and top < bottom:
-            # get every i in the top row
-            for i in range(left, right):
+        left, right = 0, len(matrix[0]) - 1
+        top, bottom = 0, len(matrix) - 1
+
+        while left <= right and top <= bottom:
+            # Traverse top row
+            for i in range(left, right + 1):
                 res.append(matrix[top][i])
             top += 1
-            # get every i in the right col
-            for i in range(top, bottom):
-                res.append(matrix[i][right - 1])
+
+            # Traverse right column
+            for i in range(top, bottom + 1):
+                res.append(matrix[i][right])
             right -= 1
-            if not (left < right and top < bottom):
-                break
-            # get every i in the bottom row
-            for i in range(right - 1, left - 1, -1):
-                res.append(matrix[bottom - 1][i])
-            bottom -= 1
-            # get every i in the left col
-            for i in range(bottom - 1, top - 1, -1):
-                res.append(matrix[i][left])
-            left += 1
+
+            if top <= bottom:
+                # Traverse bottom row
+                for i in range(right, left - 1, -1):
+                    res.append(matrix[bottom][i])
+                bottom -= 1
+
+            if left <= right:
+                # Traverse left column
+                for i in range(bottom, top - 1, -1):
+                    res.append(matrix[i][left])
+                left += 1
 
         return res
+
+def main():
+    solution = Solution()
+    n = int(input("Enter the number of rows: "))
+    m = int(input("Enter the number of columns: "))
+    matrix = []
+    print("Enter the elements of the matrix:")
+    for i in range(n):
+        row = list(map(int, input().split()))
+        matrix.append(row)
+
+    result = solution.spiralOrder(matrix)
+    print("Spiral order of the matrix:", result)
+
+if __name__ == "__main__":
+    main()
