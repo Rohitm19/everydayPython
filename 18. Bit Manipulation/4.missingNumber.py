e35a0d9a8b134f9d5d67df390dfa216fb1683e58
Rohitm19 <mudaliar.extc.engg@gmail.com>
2023-10-08 15:21:32 -0400

    missingNumber (vsCode)

--- 18. Bit Manipulation/4.missingNumber.py
+++ 18. Bit Manipulation/4.missingNumber.py
@@ -1,9 +1,20 @@
-#neetCode
+#vsCode
+
+from typing import List
 
 class Solution:
     def missingNumber(self, nums: List[int]) -> int:
-        res = len(nums)
+        n = len(nums)
+        expected_sum = n * (n + 1) // 2
+        actual_sum = sum(nums)
+        return expected_sum - actual_sum
 
-        for i in range(len(nums)):
-            res += i - nums[i]
-        return res
+def main():
+    solution = Solution()
+    nums_str = input("Enter the list of numbers separated by commas: ")
+    nums = [int(num) for num in nums_str.split(",")]
+    result = solution.missingNumber(nums)
+    print("Missing number is:", result)
+
+if __name__ == "__main__":
+    main()
