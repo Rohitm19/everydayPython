38d82a2b156570b5b1f5902374a27df3ef98a9ed
Rohitm19 <mudaliar.extc.engg@gmail.com>
2023-10-08 15:24:22 -0400

    sumofTwoIntegers (vsCode)

--- 18. Bit Manipulation/5.sumofTwoIntegers.py
+++ 18. Bit Manipulation/5.sumofTwoIntegers.py
@@ -1,4 +1,4 @@
-#neetCode
+#vsCode
 
 class Solution:
     def getSum(self, a: int, b: int) -> int:
@@ -7,12 +7,22 @@ class Solution:
                 return a or b
             return add(a ^ b, (a & b) << 1)
 
-        if a * b < 0:  # assume a < 0, b > 0
+        if a * b < 0:
             if a > 0:
                 return self.getSum(b, a)
-            if add(~a, 1) == b:  # -a == b
+            if add(~a, 1) == b:
                 return 0
-            if add(~a, 1) < b:  # -a < b
-                return add(~add(add(~a, 1), add(~b, 1)), 1)  # -add(-a, -b)
+            if add(~a, 1) < b:
+                return add(~add(add(~a, 1), add(~b, 1)), 1)
 
-        return add(a, b)  # a*b >= 0 or (-a) > b > 0
+        return add(a, b)
+
+def main():
+    solution = Solution()
+    a = int(input("Enter the value of a: "))
+    b = int(input("Enter the value of b: "))
+    result = solution.getSum(a, b)
+    print("Sum:", result)
+
+if __name__ == "__main__":
+    main()
