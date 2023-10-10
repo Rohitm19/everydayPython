cb376f1b0442bcec01dbad211c17065ffc9905e1
Rohitm19 <mudaliar.extc.engg@gmail.com>
2023-10-09 23:27:23 -0400

    validSudoku (vsCode)

--- 1. Arrays & Hashing/9.validSudoku.py
+++ 1. Arrays & Hashing/9.validSudoku.py
@@ -1,4 +1,4 @@
-#neetCode
+#vsCode
 
 class Solution:
     def isValidSudoku(self, board: List[List[str]]) -> bool:
@@ -8,16 +8,33 @@ class Solution:
 
         for r in range(9):
             for c in range(9):
-                if board[r][c] == ".":
+                cell_val = input(f"Enter the value for cell [{r}][{c}] (or '.' for empty cell): ")
+                if cell_val == ".":
                     continue
                 if (
-                    board[r][c] in rows[r]
-                    or board[r][c] in cols[c]
-                    or board[r][c] in squares[(r // 3, c // 3)]
+                    cell_val in rows[r]
+                    or cell_val in cols[c]
+                    or cell_val in squares[(r // 3, c // 3)]
                 ):
                     return False
-                cols[c].add(board[r][c])
-                rows[r].add(board[r][c])
-                squares[(r // 3, c // 3)].add(board[r][c])
+                cols[c].add(cell_val)
+                rows[r].add(cell_val)
+                squares[(r // 3, c // 3)].add(cell_val)
 
         return True
+
+def main():
+    solution = Solution()
+    board = []
+    print("Enter the Sudoku board:")
+    for i in range(9):
+        row = input().split()
+        board.append(row)
+    is_valid = solution.isValidSudoku(board)
+    if is_valid:
+        print("The Sudoku board is valid.")
+    else:
+        print("The Sudoku board is not valid.")
+
+if __name__ == "__main__":
+    main()
