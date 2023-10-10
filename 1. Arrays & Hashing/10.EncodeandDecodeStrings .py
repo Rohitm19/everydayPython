861792bcac90ee89bc9875f0e23f264d839c708c
Rohitm19 <mudaliar.extc.engg@gmail.com>
2023-10-09 23:36:52 -0400

    EncodeandDecodeStrings (vsCode)

--- 1. Arrays & Hashing/10.EncodeandDecodeStrings .py
+++ 1. Arrays & Hashing/10.EncodeandDecodeStrings .py
@@ -1,4 +1,4 @@
-#neetCode
+#vsCode
 
 class Codec:
     def encode(self, strs):
@@ -19,3 +19,19 @@ class Codec:
             i = j
             
         return res
+
+def main():
+    codec = Codec()
+
+    # Encode a list of strings
+    input_strings = input("Enter a list of strings (comma-separated): ").split(',')
+    encoded_string = codec.encode(input_strings)
+    print("Encoded string:", encoded_string)
+
+    # Decode a string
+    encoded_input = input("Enter the encoded string: ")
+    decoded_strings = codec.decode(encoded_input)
+    print("Decoded strings:", decoded_strings)
+
+if __name__ == "__main__":
+    main()
