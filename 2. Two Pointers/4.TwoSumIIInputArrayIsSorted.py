fa7f62ebe28458ba6f62f2d8118bae484f73b105
Rohitm19 <mudaliar.extc.engg@gmail.com>
2023-10-10 23:58:52 -0400

    TwoSumIIInputArrayIsSorted (vsCode)

--- 2. Two Pointers/4.TwoSumIIInputArrayIsSorted.py
+++ 2. Two Pointers/4.TwoSumIIInputArrayIsSorted.py
@@ -11,3 +11,13 @@ class Solution:
                 l += 1
             else:
                 return [l + 1, r + 1]
+
+def main():
+    solution = Solution()
+    numbers = [int(x) for x in input("Enter a list of numbers (comma-separated): ").split(',')]
+    target = int(input("Enter the target sum: "))
+    result = solution.twoSum(numbers, target)
+    print("Indices of the two numbers:", result)
+
+if __name__ == "__main__":
+    main()
