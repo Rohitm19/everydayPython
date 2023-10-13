649d65890c918c8866de6e69ef1ff0a3cb308f4c
Rohitm19 <mudaliar.extc.engg@gmail.com>
2023-10-13 18:35:36 -0400

    dailyTemperatures (vsCode)

--- 4. Stack/5.dailyTemperatures.py
+++ 4. Stack/5.dailyTemperatures.py
@@ -1,4 +1,4 @@
-#neetCode
+#vsCode
 
 class Solution:
     def dailyTemperatures(self, temperatures: List[int]) -> List[int]:
@@ -11,3 +11,15 @@ class Solution:
                 res[stackInd] = i - stackInd
             stack.append((t, i))
         return res
+
+# Manual input and usage
+solution = Solution()
+
+# Manually input temperatures
+temperatures = [73, 74, 75, 71, 69, 72, 76, 73]
+
+# Calculate daily temperatures for the given temperature list
+daily_temperatures = solution.dailyTemperatures(temperatures)
+
+# Print the daily temperatures
+print("Daily Temperatures:", daily_temperatures)
