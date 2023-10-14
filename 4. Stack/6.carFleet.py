060e2ed357f60d8dc342c0ee124f4415d960f373
Rohitm19 <mudaliar.extc.engg@gmail.com>
2023-10-14 19:40:40 -0400

    carFleet (vsCode)

--- 4. Stack/6.carFleet.py
+++ 4. Stack/6.carFleet.py
@@ -1,4 +1,4 @@
-#neetCode
+#vsCode
 
 class Solution:
     def carFleet(self, target: int, position: List[int], speed: List[int]) -> int:
@@ -10,3 +10,17 @@ class Solution:
             if len(stack) >= 2 and stack[-1] <= stack[-2]:
                 stack.pop()
         return len(stack)
+
+# Manual input and usage
+solution = Solution()
+
+# Manually input target, position, and speed
+target = 12
+position = [10, 8, 0, 5, 3]
+speed = [2, 4, 1, 1, 3]
+
+# Calculate the number of car fleets
+car_fleets = solution.carFleet(target, position, speed)
+
+# Print the number of car fleets
+print("Number of car fleets:", car_fleets)
