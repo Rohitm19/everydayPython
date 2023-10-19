9dc71d8b4c2b87c7c22db4e26dd26ee7696dfb27
Rohitm19 <mudaliar.extc.engg@gmail.com>
2023-10-18 20:37:57 -0400

    addTwoNumbers (vsCode)

--- 6. Linked List/8.addTwoNumbers.py
+++ 6. Linked List/8.addTwoNumbers.py
@@ -1,12 +1,10 @@
-#neetCode
-
 # Definition for singly-linked list.
 # class ListNode:
 #     def __init__(self, val=0, next=None):
 #         self.val = val
 #         self.next = next
 class Solution:
-    def addTwoNumbers(self, l1: ListNode, l2: ListNode) -> ListNode:
+    def addTwoNumbers(self, l1: ListNode, l2: ListNode) -&#62; ListNode:
         dummy = ListNode()
         cur = dummy
 
@@ -27,3 +25,18 @@ class Solution:
             l2 = l2.next if l2 else None
 
         return dummy.next
+
+# Manual inputs
+l1 = ListNode(2)
+l1.next = ListNode(4)
+l1.next.next = ListNode(3)
+
+l2 = ListNode(5)
+l2.next = ListNode(6)
+l2.next.next = ListNode(4)
+
+result = Solution().addTwoNumbers(l1, l2)
+
+while result:
+    print(result.val, end=" ")
+    result = result.next
