de0baa6b5b18fbb9703ff2aabf70760373eaf62d
Rohitm19 <mudaliar.extc.engg@gmail.com>
2023-10-19 22:32:24 -0400

    diameterofBinaryTree (vsCode)

--- 7. Trees/12.diameterofBinaryTree.py
+++ 7. Trees/12.diameterofBinaryTree.py
@@ -1,11 +1,12 @@
-#neetCode
+#vsCode
 
 # Definition for a binary tree node.
-# class TreeNode:
-#     def __init__(self, val=0, left=None, right=None):
-#         self.val = val
-#         self.left = left
-#         self.right = right
+class TreeNode:
+    def __init__(self, val=0, left=None, right=None):
+        self.val = val
+        self.left = left
+        self.right = right
+
 class Solution:
     def diameterOfBinaryTree(self, root: Optional[TreeNode]) -> int:
         res = 0
@@ -23,3 +24,20 @@ class Solution:
 
         dfs(root)
         return res
+
+# Manually create a binary tree
+# You can change this part to create your own binary tree
+root = TreeNode(1)
+root.left = TreeNode(2)
+root.right = TreeNode(3)
+root.left.left = TreeNode(4)
+root.left.right = TreeNode(5)
+
+# Create an instance of the Solution class
+solution = Solution()
+
+# Calculate the diameter of the binary tree
+diameter = solution.diameterOfBinaryTree(root)
+
+# Print the diameter of the binary tree
+print("Diameter of the binary tree:", diameter)
