c8d5feda85eb7c2c19a4d1ff857e2858fa45cd94
Rohitm19 <mudaliar.extc.engg@gmail.com>
2023-10-21 17:17:55 -0400

    binaryTreeRightSideView (vsCode)

--- 5. Binary Search/14.binaryTreeRightSideView.py
+++ 5. Binary Search/14.binaryTreeRightSideView.py
@@ -1,11 +1,12 @@
-#neetcode
+#vsCode
 
 # Definition for a binary tree node.
-# class TreeNode:
-#     def __init__(self, val=0, left=None, right=None):
-#         self.val = val
-#         self.left = left
-#         self.right = right
+class TreeNode:
+    def __init__(self, val=0, left=None, right=None):
+        self.val = val
+        self.left = left
+        self.right = right
+
 class Solution:
     def rightSideView(self, root: TreeNode) -> List[int]:
         res = []
@@ -24,3 +25,20 @@ class Solution:
             if rightSide:
                 res.append(rightSide.val)
         return res
+
+# Manually create a binary tree
+# You can change this part to create your own binary tree
+root = TreeNode(1)
+root.left = TreeNode(2)
+root.right = TreeNode(3)
+root.left.right = TreeNode(5)
+root.right.right = TreeNode(4)
+
+# Create an instance of the Solution class
+solution = Solution()
+
+# Get the right side view of the binary tree
+right_side_view = solution.rightSideView(root)
+
+# Print the right side view
+print("Right Side View of the Binary Tree:", right_side_view)
