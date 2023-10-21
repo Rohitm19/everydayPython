74e1ddb4035eaf90d9aed08e73c7ab64878f3385
Rohitm19 <mudaliar.extc.engg@gmail.com>
2023-10-21 17:15:43 -0400

    balancedBinaryTree (vsCode)

--- 7. Trees/13.balancedBinaryTree.py
+++ 7. Trees/13.balancedBinaryTree.py
@@ -1,11 +1,12 @@
-#neetCode
+#vsCode
 
 # Definition for a binary tree node.
-# class TreeNode:
-#     def __init__(self, val=0, left=None, right=None):
-#         self.val = val
-#         self.left = left
-#         self.right = right
+class TreeNode:
+    def __init__(self, val=0, left=None, right=None):
+        self.val = val
+        self.left = left
+        self.right = right
+
 class Solution:
     def isBalanced(self, root: Optional[TreeNode]) -> bool:
         def dfs(root):
@@ -17,3 +18,23 @@ class Solution:
             return [balanced, 1 + max(left[1], right[1])]
 
         return dfs(root)[0]
+
+# Manually create a binary tree
+# You can change this part to create your own binary tree
+root = TreeNode(1)
+root.left = TreeNode(2)
+root.right = TreeNode(3)
+root.left.left = TreeNode(4)
+root.left.right = TreeNode(5)
+
+# Create an instance of the Solution class
+solution = Solution()
+
+# Check if the binary tree is balanced
+is_balanced = solution.isBalanced(root)
+
+# Print the result
+if is_balanced:
+    print("The binary tree is balanced.")
+else:
+    print("The binary tree is not balanced.")
