d48f23282a511286b5fee1c03915d77a40aec151
Rohitm19 <mudaliar.extc.engg@gmail.com>
2023-10-22 19:39:16 -0400

    CountGoodNodesInBinaryTree (vsCode)

--- 7. Trees/15.CountGoodNodesInBinaryTree.py
+++ 7. Trees/15.CountGoodNodesInBinaryTree.py
@@ -1,11 +1,12 @@
-#neetcode
+#vsCode
 
 # Definition for a binary tree node.
-# class TreeNode:
-#     def __init__(self, val=0, left=None, right=None):
-#         self.val = val
-#         self.left = left
-#         self.right = right
+class TreeNode:
+    def __init__(self, val=0, left=None, right=None):
+        self.val = val
+        self.left = left
+        self.right = right
+
 class Solution:
     def goodNodes(self, root: TreeNode) -> int:
         def dfs(node, maxVal):
@@ -19,3 +20,21 @@ class Solution:
             return res
 
         return dfs(root, root.val)
+
+# Manually create a binary tree
+# You can change this part to create your own binary tree
+root = TreeNode(3)
+root.left = TreeNode(1)
+root.right = TreeNode(4)
+root.left.left = TreeNode(3)
+root.right.left = TreeNode(1)
+root.right.right = TreeNode(5)
+
+# Create an instance of the Solution class
+solution = Solution()
+
+# Get the number of good nodes in the binary tree
+good_nodes_count = solution.goodNodes(root)
+
+# Print the number of good nodes
+print("Number of Good Nodes in the Binary Tree:", good_nodes_count)
