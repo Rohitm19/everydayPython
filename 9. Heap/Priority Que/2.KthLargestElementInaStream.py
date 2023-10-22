6fd7421ee1a3f32d3515efd018e966c1b44e9b68
Rohitm19 <mudaliar.extc.engg@gmail.com>
2023-10-22 19:42:49 -0400

    KthLargestElementInaStream (vsCode)

--- 9. Heap/Priority Que/2.KthLargestElementInaStream.py
+++ 9. Heap/Priority Que/2.KthLargestElementInaStream.py
@@ -1,4 +1,6 @@
-#neetCode
+#vsCode
+
+import heapq
 
 class KthLargest:
     def __init__(self, k: int, nums: List[int]):
@@ -13,3 +15,16 @@ class KthLargest:
         if len(self.minHeap) > self.k:
             heapq.heappop(self.minHeap)
         return self.minHeap[0]
+
+# Manually input the initial list and values to add
+k = 3  # Change k to your desired value
+initial_nums = [4, 5, 8, 2]  # Change the list to your desired initial numbers
+values_to_add = [3, 5, 10, 9]  # Change the list to your desired values to add
+
+# Create an instance of the KthLargest class
+kthLargest = KthLargest(k, initial_nums)
+
+# Add values one by one and print the Kth largest value after each addition
+for val in values_to_add:
+    kthLargestValue = kthLargest.add(val)
+    print(f"Kth Largest after adding {val}: {kthLargestValue}")
