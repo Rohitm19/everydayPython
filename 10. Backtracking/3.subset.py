6ba69241c9fa611f7a9fbbfa4c01c67126a7d096
Rohitm19 <mudaliar.extc.engg@gmail.com>
2023-10-25 23:55:57 -0400

    subset (vsCode)

--- 10. Backtracking/3.subset.py
+++ 10. Backtracking/3.subset.py
@@ -1,7 +1,9 @@
-#neetcode
+#vsCode
+
+from typing import List
 
 class Solution:
-    def subsets(self, nums: List[int]) -> List[List[int]]:
+    def subsets(self, nums: List[int]) -> List[List[int]:
         res = []
 
         subset = []
@@ -10,12 +12,29 @@ class Solution:
             if i >= len(nums):
                 res.append(subset.copy())
                 return
-            # decision to include nums[i]
-            subset.append(nums[i])
-            dfs(i + 1)
-            # decision NOT to include nums[i]
-            subset.pop()
-            dfs(i + 1)
+
+            # Manually input a choice (1 for include, 0 for not include) for nums[i]
+            choice = 1  # Change to 0 or 1 as needed
+            
+            if choice == 1:
+                subset.append(nums[i])
+                dfs(i + 1)
+                subset.pop()
+
+            if choice == 0:
+                dfs(i + 1)
 
         dfs(0)
         return res
+
+# Manually input your list of integers here
+nums = [1, 2, 3]
+
+# Create an instance of the Solution class
+solution = Solution()
+
+# Call the subsets method to generate subsets based on your manual input
+result = solution.subsets(nums)
+
+# Print the generated subsets
+print(result)
