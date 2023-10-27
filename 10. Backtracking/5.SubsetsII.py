30f11a87f0e491bbbda775d1b1ca1498ce78287f
Rohitm19 <mudaliar.extc.engg@gmail.com>
2023-10-26 23:54:45 -0400

    SubsetsII (vsCode)

--- 10. Backtracking/5.SubsetsII.py
+++ 10. Backtracking/5.SubsetsII.py
@@ -1,7 +1,9 @@
-#neetCode
+#vsCode
+
+from typing import List
 
 class Solution:
-    def subsetsWithDup(self, nums: List[int]) -> List[List[int]]:
+    def subsetsWithDup(self, nums: List[int]) -> List[List[int]:
         res = []
         nums.sort()
 
@@ -10,14 +12,29 @@ class Solution:
                 res.append(subset[::])
                 return
 
-            # All subsets that include nums[i]
+            # Include the current element
             subset.append(nums[i])
             backtrack(i + 1, subset)
             subset.pop()
-            # All subsets that don't include nums[i]
+
+            # Skip duplicates
             while i + 1 < len(nums) and nums[i] == nums[i + 1]:
                 i += 1
+
+            # Exclude the current element
             backtrack(i + 1, subset)
+
+        # Manually input your list of integers here
+        nums = [1, 2, 2]
 
         backtrack(0, [])
         return res
+
+# Create an instance of the Solution class
+solution = Solution()
+
+# Call the subsetsWithDup method to generate subsets based on your manual input
+result = solution.subsetsWithDup(nums)
+
+# Print the generated subsets
+print(result)
