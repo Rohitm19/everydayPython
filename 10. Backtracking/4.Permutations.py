f835537ca661264dbdc6c5de95bab6b5a6777f49
Rohitm19 <mudaliar.extc.engg@gmail.com>
2023-10-26 23:52:47 -0400

    Permutations (vsCode)

--- 10. Backtracking/4.Permutations.py
+++ 10. Backtracking/4.Permutations.py
@@ -1,19 +1,36 @@
-#neetcode
+#vsCode
+
+from typing import List
 
 class Solution:
     def permute(self, nums: List[int]) -> List[List[int]]:
         res = []
 
-        # base case
+        # Base case: If the input list contains only one element, return it as a permutation.
         if len(nums) == 1:
             return [nums[:]]  # nums[:] is a deep copy
 
         for i in range(len(nums)):
-            n = nums.pop(0)
+            n = nums.pop(i)  # Manually select an element at index 'i' to include in the permutation
+
             perms = self.permute(nums)
 
             for perm in perms:
                 perm.append(n)
             res.extend(perms)
-            nums.append(n)
+
+            nums.insert(i, n)  # Restore the list by inserting the element back at index 'i'
+
         return res
+
+# Manually input your list of integers here
+nums = [1, 2, 3]
+
+# Create an instance of the Solution class
+solution = Solution()
+
+# Call the permute method to generate permutations based on your manual input
+result = solution.permute(nums)
+
+# Print the generated permutations
+print(result)
