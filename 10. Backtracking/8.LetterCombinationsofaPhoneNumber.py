8707b45ab37ebf6bdf756c6498b531fbce073896
Rohitm19 <mudaliar.extc.engg@gmail.com>
2023-10-29 18:55:27 -0400

    LetterCombinationsofaPhoneNumber (vsCode)

--- 10. Backtracking/8.LetterCombinationsofaPhoneNumber.py
+++ 10. Backtracking/8.LetterCombinationsofaPhoneNumber.py
@@ -1,5 +1,6 @@
-#neetCode
+#vsCode
 
+from typing import List
 
 class Solution:
     def letterCombinations(self, digits: str) -> List[str]:
@@ -22,7 +23,19 @@ class Solution:
             for c in digitToChar[digits[i]]:
                 backtrack(i + 1, curStr + c)
 
+        # Manually input your digits here
+        digits = "23"  # Adjust as needed
+
         if digits:
             backtrack(0, "")
 
         return res
+
+# Create an instance of the Solution class
+solution = Solution()
+
+# Call the letterCombinations method to generate combinations based on your manual input
+result = solution.letterCombinations(digits)
+
+# Print the generated combinations
+print(result)
