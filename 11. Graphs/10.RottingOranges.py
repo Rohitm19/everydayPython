ffecff9b4ea075ccfe3a2716db8f8773d389dde7
Rohitm19 <mudaliar.extc.engg@gmail.com>
2023-10-31 23:55:29 -0400

    RottingOranges (vsCode)

--- 11. Graphs/10.RottingOranges.py
+++ 11. Graphs/10.RottingOranges.py
@@ -1,10 +1,20 @@
-#neetCode
+#vsCode
+
+from typing import List
+import collections
 
 class Solution:
     def orangesRotting(self, grid: List[List[int]]) -> int:
         q = collections.deque()
         fresh = 0
         time = 0
+
+        # Manually input the grid here
+        grid = [
+            [2, 1, 1],
+            [1, 1, 0],
+            [0, 1, 1],
+        ]  # Adjust as needed
 
         for r in range(len(grid)):
             for c in range(len(grid[0])):
@@ -21,8 +31,6 @@ class Solution:
 
                 for dr, dc in directions:
                     row, col = r + dr, c + dc
-                    # if in bounds and nonrotten, make rotten
-                    # and add to q
                     if (
                         row in range(len(grid))
                         and col in range(len(grid[0]))
@@ -33,3 +41,12 @@ class Solution:
                         fresh -= 1
             time += 1
         return time if fresh == 0 else -1
+
+# Create an instance of the Solution class
+solution = Solution()
+
+# Call the orangesRotting method to calculate the time needed based on your manual input
+result = solution.orangesRotting(grid)
+
+# Print the result
+print("Time needed:", result)
