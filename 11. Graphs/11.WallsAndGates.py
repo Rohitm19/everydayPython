d0093469b7310fc42284ac7a082ccf40f7e831c1
Rohitm19 <mudaliar.extc.engg@gmail.com>
2023-11-01 23:09:09 -0400

    WallsAndGates (vsCode)

--- 11. Graphs/11.WallsAndGates.py
+++ 11. Graphs/11.WallsAndGates.py
@@ -1,15 +1,21 @@
-#neetCode
+#vsCode
+
+from typing import List
+from collections import deque
 
 class Solution:
-    """
-    @param rooms: m x n 2D grid
-    @return: nothing
-    """
-
-    def walls_and_gates(self, rooms: List[List[int]]):
+    def wallsAndGates(self, rooms: List[List[int]) -> None:
         ROWS, COLS = len(rooms), len(rooms[0])
         visit = set()
         q = deque()
+
+        # Manually input the rooms here
+        rooms = [
+            [2147483647, -1, 0, 2147483647],
+            [2147483647, 2147483647, 2147483647, -1],
+            [2147483647, -1, 2147483647, -1],
+            [0, -1, 2147483647, 2147483647]
+        ]  # Adjust as needed
 
         def addRooms(r, c):
             if (
@@ -39,3 +45,13 @@ class Solution:
                 addRooms(r, c + 1)
                 addRooms(r, c - 1)
             dist += 1
+
+# Create an instance of the Solution class
+solution = Solution()
+
+# Call the wallsAndGates method to calculate room distances based on your manual input
+solution.wallsAndGates(rooms)
+
+# Print the updated rooms after calculating distances
+for row in rooms:
+    print(row)
