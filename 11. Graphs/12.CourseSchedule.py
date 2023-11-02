74724ea35896b842df3ee99f023b2c688b93f2ec
Rohitm19 <mudaliar.extc.engg@gmail.com>
2023-11-01 23:11:05 -0400

    CourseSchedule (vsCode)

--- 11. Graphs/12.CourseSchedule.py
+++ 11. Graphs/12.CourseSchedule.py
@@ -1,31 +1,48 @@
-#neetCode
+#vsCode
+
+from typing import List
 
 class Solution:
     def canFinish(self, numCourses: int, prerequisites: List[List[int]]) -> bool:
-        # dfs
         preMap = {i: [] for i in range(numCourses)}
 
-        # map each course to : prereq list
-        for crs, pre in prerequisites:
-            preMap[crs].append(pre)
+        # Manually input the prerequisites here
+        prerequisites = [[1, 0], [2, 1], [3, 2]]  # Adjust as needed
+
+        # Map each course to its prerequisite list
+        for course, prereq in prerequisites:
+            preMap[course].append(prereq)
 
         visiting = set()
 
-        def dfs(crs):
-            if crs in visiting:
+        def dfs(course):
+            if course in visiting:
                 return False
-            if preMap[crs] == []:
+            if not preMap[course]:
                 return True
 
-            visiting.add(crs)
-            for pre in preMap[crs]:
-                if not dfs(pre):
+            visiting.add(course)
+            for prereq in preMap[course]:
+                if not dfs(prereq):
                     return False
-            visiting.remove(crs)
-            preMap[crs] = []
+            visiting.remove(course)
+            preMap[course] = []
             return True
 
-        for c in range(numCourses):
-            if not dfs(c):
+        for course in range(numCourses):
+            if not dfs(course):
                 return False
         return True
+
+# Create an instance of the Solution class
+solution = Solution()
+
+# Call the canFinish method to check if it's possible to finish the courses based on your manual input
+numCourses = 4  # Adjust the number of courses as needed
+result = solution.canFinish(numCourses, prerequisites)
+
+# Print the result
+if result:
+    print("It is possible to finish the courses.")
+else:
+    print("It is not possible to finish the courses.")
