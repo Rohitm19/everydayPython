2c062d8dff3f2aedd5409c4a06fb1589eb2032d2
Rohitm19 <mudaliar.extc.engg@gmail.com>
2023-11-15 21:54:19 -0500

    MergeTripletstoFormTargetTriplet (vsCode)

--- 15. Greedy/6.MergeTripletstoFormTargetTriplet.py
+++ 15. Greedy/6.MergeTripletstoFormTargetTriplet.py
@@ -1,4 +1,4 @@
-#neetCode
+#vsCode
 
 class Solution:
     def mergeTriplets(self, triplets: List[List[int]], target: List[int]) -> bool:
@@ -11,3 +11,16 @@ class Solution:
                 if v == target[i]:
                     good.add(i)
         return len(good) == 3
+
+# Manual Input
+triplets = [[1, 2, 3], [4, 5, 6], [7, 8, 9]]
+target = [4, 5, 6]
+
+# Create an instance of the Solution class
+solution = Solution()
+
+# Call the mergeTriplets method with your input
+result = solution.mergeTriplets(triplets, target)
+
+# Print the result
+print("Is it possible to merge triplets:", result)
