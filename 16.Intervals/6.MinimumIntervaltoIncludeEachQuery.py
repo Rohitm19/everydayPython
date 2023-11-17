e75f508d70581e4aa1537d5eb4b4361c6cd4c25b
Rohitm19 <mudaliar.extc.engg@gmail.com>
2023-11-17 16:51:44 -0500

    MinimumIntervaltoIncludeEachQuery (vsCode)

--- 16.Intervals/6.MinimumIntervaltoIncludeEachQuery.py
+++ 16.Intervals/6.MinimumIntervaltoIncludeEachQuery.py
@@ -1,4 +1,7 @@
-#neetCode
+#vsCode
+
+from typing import List
+import heapq
 
 class Solution:
     def minInterval(self, intervals: List[List[int]], queries: List[int]) -> List[int]:
@@ -16,3 +19,12 @@ class Solution:
                 heapq.heappop(minHeap)
             res[q] = minHeap[0][0] if minHeap else -1
         return [res[q] for q in queries]
+
+# Manual Input
+intervals_input = [[1, 4], [2, 4], [3, 6]]
+queries_input = [2, 3, 4, 5]
+solution = Solution()
+
+# Using the method
+result = solution.minInterval(intervals_input, queries_input)
+print("Result:", result)
