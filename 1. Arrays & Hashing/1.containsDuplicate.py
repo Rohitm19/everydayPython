f88ee7ec380cb4600816dfa6de3ca07e56d2e90d
Rohitm19 <mudaliar.extc.engg@gmail.com>
2023-11-21 23:36:46 -0500

    containsDuplicate (neetCode&vsCode)

--- 1. Arrays & Hashing/1.containsDuplicate.py
+++ 1. Arrays & Hashing/1.containsDuplicate.py
@@ -1,3 +1,14 @@
+#neetCode
+class Solution:
+    def containsDuplicate(self, nums: List[int]) -> bool:
+        hashset = set()
+
+        for n in nums:
+            if n in hashset:
+                return True
+            hashset.add(n)
+        return False
+
 #VS Code
 from typing import List
 def containsDuplicate(nums: List[int]) -> bool:  # main logic
--- 1. Arrays & Hashing/2.validAnagram.py
+++ 1. Arrays & Hashing/2.validAnagram.py
@@ -9,14 +9,17 @@ class Solution:                                       #main logic
         countS, countT = {}, {}                      # creating hashmap
 
         for i in range(len(s)):
-            print(s)
+            #print(s)
+           # print(countS)
             countS[s[i]] = 1 + countS.get(s[i], 0)     # we can access the value of a dictionary with the syntax dictionary[key]. In this case, the key is the character s[i]. So, the line countS[s[i]] is accessing the value of the dictionary countS for the key s[i]. 
+            #print(s[i])
             print(countS)                              # so  countS[s[i]] = r for first iteration
                                                        # &  1 + countS.get(s[i], 0) = 1 . This tries to get the s[i] from the dictonary countS. 
             countT[t[i]] = 1 + countT.get(t[i], 0)
-        
+            print(countT)
+        #print (countS == countT)
         return countS == countT                        #The return countS == countT line returns True if the two dictionaries countS and countT are equal. Otherwise, the function returns False
- 
+          
 def main():                                            # logic for manual input
     s = input("Enter the first string: ")
     t = input("Enter the second string: ")
