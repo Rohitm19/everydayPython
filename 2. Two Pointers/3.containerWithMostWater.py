9592c1a71de88c0d95a6076b8ad91239eb098ec0
Rohitm19 <mudaliar.extc.engg@gmail.com>
2023-11-24 15:59:07 -0500

    containerWithMostWater (neetcode & vsCode)

--- 2. Two Pointers/3.containerWithMostWater.py
+++ 2. Two Pointers/3.containerWithMostWater.py
@@ -1,5 +1,20 @@
-#vsCode
+#neetCode
+class Solution:
+    def maxArea(self, height: List[int]) -> int:
+        l, r = 0, len(height) - 1
+        res = 0
 
+        while l < r:
+            res = max(res, min(height[l], height[r]) * (r - l))
+            if height[l] < height[r]:
+                l += 1
+            elif height[r] <= height[l]:
+                r -= 1
+            
+        return res
+
+
+#vsCode
 class Solution:
     def maxArea(self, height):
         l, r = 0, len(height) - 1
