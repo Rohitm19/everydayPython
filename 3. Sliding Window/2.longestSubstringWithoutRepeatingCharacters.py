41d5423a6e46e5161f04f387e25a86f4c9c061d7
Rohitm19 <mudaliar.extc.engg@gmail.com>
2023-11-26 22:50:01 -0500

    longestSubstringWithoutRepeatingCharacters (neetCode & vsCode)

--- 3. Sliding Window/2.longestSubstringWithoutRepeatingCharacters.py
+++ 3. Sliding Window/2.longestSubstringWithoutRepeatingCharacters.py
@@ -1,5 +1,19 @@
-#vsCode
+#neeetCode
+class Solution:
+    def lengthOfLongestSubstring(self, s: str) -> int:
+        charSet = set()
+        l = 0
+        res = 0
 
+        for r in range(len(s)):
+            while s[r] in charSet:
+                charSet.remove(s[l])
+                l += 1
+            charSet.add(s[r])
+            res = max(res, r - l + 1)
+        return res
+
+#vsCode
 class Solution:
     def lengthOfLongestSubstring(self, s: str) -> int:
         charSet = set()
