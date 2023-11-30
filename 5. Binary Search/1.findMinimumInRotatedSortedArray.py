2ccd7c816e537ec5b5bd7003e24a12e963d1eae9
Rohitm19 <mudaliar.extc.engg@gmail.com>
2023-11-29 23:52:00 -0500

    findMinimumInRotatedSortedArray (neetCode & vsCode)

--- 5. Binary Search/1.findMinimumInRotatedSortedArray.py
+++ 5. Binary Search/1.findMinimumInRotatedSortedArray.py
@@ -1,17 +1,23 @@
 #neetCode
 class Solution:
-    def search(self, nums: List[int], target: int) -> int:
-        l, r = 0, len(nums) - 1
-
-        while l <= r:
-            m = l + ((r - l) // 2)  # (l + r) // 2 can lead to overflow
-            if nums[m] > target:
-                r = m - 1
-            elif nums[m] < target:
-                l = m + 1
+    def findMin(self, nums: List[int]) -> int:
+        start , end = 0, len(nums) - 1 
+        curr_min = float("inf")
+        
+        while start  <  end :
+            mid = (start + end ) // 2
+            curr_min = min(curr_min,nums[mid])
+            
+            # right has the min 
+            if nums[mid] > nums[end]:
+                start = mid + 1
+                
+            # left has the  min 
             else:
-                return m
-        return -1
+                end = mid - 1 
+                
+        return min(curr_min,nums[start])
+
 
 #vsCode
 
