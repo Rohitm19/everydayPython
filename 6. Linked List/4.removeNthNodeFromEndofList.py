3cc5cb0e06f377075205d232b09b65659d2fc774
Rohitm19 <mudaliar.extc.engg@gmail.com>
2023-12-03 23:49:50 -0500

    removeNthNodeFromEndofList (neetCode & vsCode)

--- 6. Linked List/4.removeNthNodeFromEndofList.py
+++ 6. Linked List/4.removeNthNodeFromEndofList.py
@@ -1,6 +1,23 @@
+#neetCode
+class Solution:
+    def removeNthFromEnd(self, head: ListNode, n: int) -> ListNode:
+        dummy = ListNode(0, head)
+        left = dummy
+        right = head
+
+        while n > 0:
+            right = right.next
+            n -= 1
+
+        while right:
+            left = left.next
+            right = right.next
+
+        # delete
+        left.next = left.next.next
+        return dummy.next
+
 #vsCode
-
-
 class ListNode:
     def __init__(self, val=0, next=None):
         self.val = val
