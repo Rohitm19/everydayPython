6373edf96dd06b003b6ce8bea67e0f82b568841b
Rohitm19 <mudaliar.extc.engg@gmail.com>
2023-12-10 23:55:19 -0500

    ConstructBinaryTreeFromPreorderAndInorderTraversal (neetCode & vsCode)

--- 7. Trees/9.ConstructBinaryTreeFromPreorderAndInorderTraversal.py
+++ 7. Trees/9.ConstructBinaryTreeFromPreorderAndInorderTraversal.py
@@ -1,3 +1,15 @@
+#neetCode
+class Solution:
+    def buildTree(self, preorder: List[int], inorder: List[int]) -> Optional[TreeNode]:
+        if not preorder or not inorder:
+            return None
+
+        root = TreeNode(preorder[0])
+        mid = inorder.index(preorder[0])
+        root.left = self.buildTree(preorder[1 : mid + 1], inorder[:mid])
+        root.right = self.buildTree(preorder[mid + 1 :], inorder[mid + 1 :])
+        return root
+
 #vsCode
 
 from typing import List, Optional
