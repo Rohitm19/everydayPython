3c2d24776a215d7f5afeaa61ccea4fa9b726b2e8
Rohitm19 <mudaliar.extc.engg@gmail.com>
2023-12-22 23:30:16 -0500

    AlienDictionary (neetCode & vsCode)

--- 12. Advanced Graphs/5.AlienDictionary.py
+++ 12. Advanced Graphs/5.AlienDictionary.py
@@ -37,3 +37,53 @@ class Solution:
 
         res.reverse()
         return "".join(res)
+
+#vsCode
+    from typing import List
+
+class Solution:
+    def alienOrder(self, words: List[str]) -> str:
+        # Manual Input for testing
+        words_input = input("Enter the words separated by space: ").split()
+
+        adj = {char: set() for word in words_input for char in word}
+
+        for i in range(len(words_input) - 1):
+            w1, w2 = words_input[i], words_input[i + 1]
+            minLen = min(len(w1), len(w2))
+            if len(w1) > len(w2) and w1[:minLen] == w2[:minLen]:
+                return ""
+            for j in range(minLen):
+                if w1[j] != w2[j]:
+                    adj[w1[j]].add(w2[j])
+                    break
+
+        visited = {}  # {char: bool} False visited, True current path
+        res = []
+
+        def dfs(char):
+            if char in visited:
+                return visited[char]
+
+            visited[char] = True
+
+            for neighChar in adj[char]:
+                if dfs(neighChar):
+                    return True
+
+            visited[char] = False
+            res.append(char)
+
+        for char in adj:
+            if dfs(char):
+                return ""
+
+        res.reverse()
+        return "".join(res)
+
+# Example Usage
+solution = Solution()
+result = solution.alienOrder(["wrt", "wrf", "er", "ett", "rftt"])
+
+# Printing the result
+print(result)
