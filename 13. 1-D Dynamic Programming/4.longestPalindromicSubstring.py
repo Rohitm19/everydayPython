e59e95abc4485db105245aac031f043c0b6bca98
Rohitm19 <mudaliar.extc.engg@gmail.com>
2023-12-24 12:12:24 -0500

    longestPalindromicSubstring (neetCode & vsCode)

--- 13. 1-D Dynamic Programming/4.longestPalindromicSubstring.py
+++ 13. 1-D Dynamic Programming/4.longestPalindromicSubstring.py
@@ -1,5 +1,18 @@
-#vsCode
+#neetCode
+class Solution:
+    def longestPalindrome(self, s: str) -> str:
+        res = ""
 
+        for i in range(len(s)):
+            for l, r in ((i,i), (i,i+1)): # odd and even lengths
+                while l >= 0 and r < len(s) and s[l] == s[r]:
+                    if (r - l + 1) > len(res):
+                        res = s[l:r + 1]
+                    l -= 1
+                    r += 1
+
+        return res
+#vsCode
 class Solution:
     def longestPalindrome(self, s: str) -> str:
         res = ""
