217ffc84031c7615659ff61403dc7a26ee434923
Rohitm19 <mudaliar.extc.engg@gmail.com>
2023-12-28 04:43:59 -0500

    maximumSubarray (neetCode & vsCode)

--- 15. Greedy/1.maximumSubarray.py
+++ 15. Greedy/1.maximumSubarray.py
@@ -1,5 +1,17 @@
-#vsCode
+#neetCode
+class Solution:
+    def maxSubArray(self, nums: List[int]) -> int:
+        res = nums[0]
 
+        total = 0
+        for n in nums:
+            total += n
+            res = max(res, total)
+            if total < 0:
+                total = 0
+        return res
+
+#vsCode
 from typing import List
 
 class Solution:
