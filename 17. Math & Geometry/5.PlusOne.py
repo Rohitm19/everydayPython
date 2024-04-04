0fc4e860a817649895be5ba57007c22885ba09db
Rohitm19 <mudaliar.extc.engg@gmail.com>
2024-04-03 22:14:25 -0400

    Plus One (neetCode & vsCode)

--- 17. Math & Geometry/5.PlusOne.py
+++ 17. Math & Geometry/5.PlusOne.py
@@ -1,3 +1,23 @@
+#neetyCode
+class Solution:
+    def plusOne(self, digits: List[int]) -> List[int]:
+        one = 1
+        i = 0
+        digits = digits[::-1]
+
+        while one:
+            if i < len(digits):
+                if digits[i] == 9:
+                    digits[i] = 0
+                else:
+                    digits[i] += 1
+                    one = 0
+            else:
+                digits.append(one)
+                one = 0
+            i += 1
+        return digits[::-1]
+
 #vsCode
 
 from typing import List
@@ -28,3 +48,4 @@ solution = Solution()
 # Using the method
 result = solution.plusOne(input_digits)
 print("Result:", result)
+
