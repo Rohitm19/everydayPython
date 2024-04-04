caff1e5f9d325a00ea0ea7f1fe4dbc170cbbc5c5
Rohitm19 <mudaliar.extc.engg@gmail.com>
2024-04-04 14:00:56 -0400

    reverseBits (neetCode & vsCode)

--- 18. Bit Manipulation/3.reverseBits.py
+++ 18. Bit Manipulation/3.reverseBits.py
@@ -1,3 +1,12 @@
+#neetCode
+class Solution:
+    def reverseBits(self, n: int) -> int:
+        res = 0
+        for i in range(32):
+            bit = (n >> i) & 1
+            res += (bit << (31 - i))
+        return res
+
 #vsCode
 
 class Solution:
