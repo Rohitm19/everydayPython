329bed80f394ad33c8618698baf9bff9d77d1a38
Rohitm19 <mudaliar.extc.engg@gmail.com>
2024-04-06 22:55:55 -0400

    containsDuplicate (neetCode)

--- 1. Arrays & Hashing/1.containsDuplicate.py
+++ 1. Arrays & Hashing/1.containsDuplicate.py
@@ -9,23 +9,3 @@ class Solution:
             hashset.add(n)
         return False
 
-#VS Code
-from typing import List
-def containsDuplicate(nums: List[int]) -> bool:  # main logic
-        hashset = set()
-
-        for n in nums:
-            if n in hashset:
-                return True
-            hashset.add(n)
-        return False
-
-        
-while True:                                                           # this part is to take input from command line
-    nums_str = input("Enter a list of ints separated by commas: ")
-    nums = list(map(int, nums_str.split(",")))
-    if nums and nums != []:
-        break
-
-print(containsDuplicate(nums))
-
