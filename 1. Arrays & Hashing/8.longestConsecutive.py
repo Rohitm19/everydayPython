c6bef35e8ef37c37b2e694378cc5570e3b6c4bbc
Rohitm19 <mudaliar.extc.engg@gmail.com>
2024-04-07 23:41:19 -0400

    longestConsecutive (neetCode)

--- 1. Arrays & Hashing/8.longestConsecutive.py
+++ 1. Arrays & Hashing/8.longestConsecutive.py
@@ -13,38 +13,3 @@ class Solution:
                     length += 1
                 longest = max(length, longest)
         return longest
-
-
-#vsCode
-import sys
-from typing import List
-
-class Solution:
-    def longestConsecutive(self, nums: List[int]) -> int:
-        numSet = set(nums)
-        longest = 0
-
-        for n in nums:
-            # check if its the start of a sequence
-            if (n - 1) not in numSet:
-                length = 1
-                while (n + length) in numSet:
-                    length += 1
-                longest = max(length, longest)
-        return longest
-
-def main():
-    nums = []
-    while True:
-        n = input("Enter a number: ")
-        if n == "":
-            break
-        nums.append(int(n))
-
-    solution = Solution()
-    result = solution.longestConsecutive(nums)
-
-    print("The longest consecutive sequence is of length: {}".format(result))
-
-if __name__ == "__main__":
-    main()
