2601f5c2bbd96f96b1f86837005d531f7f42dacb
Rohitm19 <mudaliar.extc.engg@gmail.com>
2024-04-09 21:55:00 -0400

    bestTimeToBuySellStock (neetCode)

--- 3. Sliding Window/1.bestTimeToBuySellStock.py
+++ 3. Sliding Window/1.bestTimeToBuySellStock.py
@@ -9,29 +9,3 @@ class Solution:
                 lowest = price
             res = max(res, price - lowest)
         return res
-
-#vsCode
-class Solution:
-    def maxProfit(self, prices: List[int]) -> int:
-        res = 0
-        
-        lowest = prices[0]
-        for price in prices:
-            if price < lowest:
-                lowest = price
-            res = max(res, price - lowest)
-        return res
-
-def main():
-    num_prices = int(input("Enter the number of prices: "))
-    prices = []
-    for _ in range(num_prices):
-        price = int(input("Enter a price: "))
-        prices.append(price)
-
-    solution = Solution()
-    max_profit = solution.maxProfit(prices)
-    print("Maximum profit:", max_profit)
-
-if __name__ == "__main__":
-    main()
