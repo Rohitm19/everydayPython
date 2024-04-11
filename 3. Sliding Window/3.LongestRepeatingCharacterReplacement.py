e32fbf2e4b98ec72c90efa997dadaeae9606e150
Rohitm19 <mudaliar.extc.engg@gmail.com>
2024-04-10 21:27:54 -0400

    LongestRepeatingCharacterReplacement (neetCode)

--- 3. Sliding Window/3.LongestRepeatingCharacterReplacement.py
+++ 3. Sliding Window/3.LongestRepeatingCharacterReplacement.py
@@ -14,30 +14,3 @@ class Solution:
                 l += 1
 
         return (r - l + 1)
-
-#vsCode
-class Solution:
-    def characterReplacement(self, s: str, k: int) -> int:
-        count = {}
-        
-        l = 0
-        maxf = 0
-        for r in range(len(s)):
-            count[s[r]] = 1 + count.get(s[r], 0)
-            maxf = max(maxf, count[s[r]])
-
-            if (r - l + 1) - maxf > k:
-                count[s[l]] -= 1
-                l += 1
-
-        return (r - l + 1)
-
-def main():
-    input_string = input("Enter a string: ")
-    k = int(input("Enter the value of k: "))
-    solution = Solution()
-    result = solution.characterReplacement(input_string, k)
-    print("Length of longest substring after character replacement:", result)
-
-if __name__ == "__main__":
-    main()
