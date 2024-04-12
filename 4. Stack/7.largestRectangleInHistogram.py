83858442399e3db53b477e7c03db39c2bf3f321d
Rohitm19 <mudaliar.extc.engg@gmail.com>
2024-04-12 15:54:38 -0400

    largestRectangleInHistogram (neetCode)

--- 4. Stack/7.largestRectangleInHistogram.py
+++ 4. Stack/7.largestRectangleInHistogram.py
@@ -15,32 +15,3 @@ class Solution:
         for i, h in stack:
             maxArea = max(maxArea, h * (len(heights) - i))
         return maxArea
-
-#vsCode
-from typing import List
-
-class Solution:
-    def largestRectangleArea(self, heights: List[int]) -> int:
-        maxArea = 0
-        stack = []  # pair: (index, height)
-
-        for i, h in enumerate(heights):
-            start = i
-            while stack and stack[-1][1] > h:
-                index, height = stack.pop()
-                maxArea = max(maxArea, height * (i - index))
-                start = index
-            stack.append((start, h))
-
-        for i, h in stack:
-            maxArea = max(maxArea, h * (len(heights) - i))
-        return maxArea
-
-# Manual input of heights
-heights_input = input("Enter the heights separated by spaces: ")
-heights = [int(height) for height in heights_input.split()]
-
-# Calculate the largest rectangle area
-solution = Solution()
-largest_area = solution.largestRectangleArea(heights)
-print("Largest Rectangle Area:", largest_area)
