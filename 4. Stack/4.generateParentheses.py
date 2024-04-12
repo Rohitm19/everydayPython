066d3958d53a094bd2dcbafb1f7da1d38aa3bff2
Rohitm19 <mudaliar.extc.engg@gmail.com>
2024-04-11 23:06:05 -0400

    generateParentheses (neetCode)

--- 4. Stack/4.generateParentheses.py
+++ 4. Stack/4.generateParentheses.py
@@ -20,39 +20,3 @@ class Solution:
 
         backtrack(0, 0)
         return res
-
-#vsCode
-
-class Solution:
-    def generateParenthesis(self, n: int) -> List[str]:
-        stack = []
-        res = []
-
-        def backtrack(openN, closedN):
-            if openN == closedN == n:
-                res.append("".join(stack))
-                return
-
-            if openN < n:
-                stack.append("(")
-                backtrack(openN + 1, closedN)
-                stack.pop()
-            if closedN < openN:
-                stack.append(")")
-                backtrack(openN, closedN + 1)
-                stack.pop()
-
-        backtrack(0, 0)
-        return res
-
-# Manual input and usage
-solution = Solution()
-
-# Manually input n
-n = 3
-
-# Generate parentheses for the given n
-parentheses = solution.generateParenthesis(n)
-
-# Print the generated parentheses
-print("Generated Parentheses for n =", n, ":", parentheses)
