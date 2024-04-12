d5f661814d1a0ae43e4cb947b65070eec39aecc5
Rohitm19 <mudaliar.extc.engg@gmail.com>
2024-04-11 23:05:41 -0400

    evaluateReversePolishNotation (neetCode)

--- 4. Stack/3.evaluateReversePolishNotation.py
+++ 4. Stack/3.evaluateReversePolishNotation.py
@@ -16,37 +16,3 @@ class Solution:
             else:
                 stack.append(int(c))
         return stack[0]
-
-
-#vsCode
-
-class Solution:
-    def evalRPN(self, tokens: List[str]) -> int:
-        stack = []
-        for c in tokens:
-            if c == "+":
-                stack.append(stack.pop() + stack.pop())
-            elif c == "-":
-                a, b = stack.pop(), stack.pop()
-                stack.append(b - a)
-            elif c == "*":
-                stack.append(stack.pop() * stack.pop())
-            elif c == "/":
-                a, b = stack.pop(), stack.pop()
-                stack.append(int(float(b) / a))
-            else:
-                stack.append(int(c))
-        return stack[0]
-
-# Manual input and usage
-solution = Solution()
-
-# Example 1
-expression1 = ["2", "1", "+", "3", "*"]
-result1 = solution.evalRPN(expression1)
-print("Result for expression1:", result1)
-
-# Example 2
-expression2 = ["4", "13", "5", "/", "+"]
-result2 = solution.evalRPN(expression2)
-print("Result for expression2:", result2)
