de6ed6e49b982f64b6150f66a4865e87a0e09ac3
Rohitm19 <mudaliar.extc.engg@gmail.com>
2024-04-13 14:23:36 -0400

    kokoEatingBananas (neetCode)

--- 5. Binary Search/5.kokoEatingBananas.py
+++ 5. Binary Search/5.kokoEatingBananas.py
@@ -16,35 +16,3 @@ class Solution:
             else:
                 l = k + 1
         return res
-
-#vsCode
-
-from typing import List
-import math
-
-class Solution:
-    def minEatingSpeed(self, piles: List[int], h: int) -> int:
-        l, r = 1, max(piles)
-        res = r
-
-        while l <= r:
-            k = (l + r) // 2
-
-            totalTime = 0
-            for p in piles:
-                totalTime += math.ceil(float(p) / k)
-            if totalTime <= h:
-                res = k
-                r = k - 1
-            else:
-                l = k + 1
-        return res
-
-# Manual input of the piles and h
-piles = [int(x) for x in input("Enter the piles (separated by spaces): ").split()]
-h = int(input("Enter the number of hours available: "))
-
-# Calculate the minimum eating speed
-solution = Solution()
-min_speed = solution.minEatingSpeed(piles, h)
-print("Minimum eating speed:", min_speed)
