4084a8b6d1c3d3167918715cb1b7a7dc5619ac41
Rohitm19 <mudaliar.extc.engg@gmail.com>
2024-04-16 13:12:50 -0400

    findTheDuplicateNumber (neetCode)

--- 6. Linked List/9.findTheDuplicateNumber.py
+++ 6. Linked List/9.findTheDuplicateNumber.py
@@ -14,35 +14,3 @@ class Solution:
             slow2 = nums[slow2]
             if slow == slow2:
                 return slow
-
-#vsCode
-
-from typing import List
-
-class Solution:
-    def findDuplicate(self, nums: List[int]) -> int:
-        slow, fast = 0, 0
-        while True:
-            slow = nums[slow]
-            fast = nums[nums[fast]]
-            if slow == fast:
-                break
-
-        slow2 = 0
-        while True:
-            slow = nums[slow]
-            slow2 = nums[slow2]
-            if slow == slow2:
-                return slow
-
-# Manual input and usage
-solution = Solution()
-
-# Manually input the list of numbers
-nums = [1, 3, 4, 2, 2]
-
-# Find the duplicate
-duplicate = solution.findDuplicate(nums)
-
-# Print the duplicate
-print("Duplicate number:", duplicate)
