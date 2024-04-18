7d071015b632cc51dcf50be99cd29c46307c16e7
Rohitm19 <mudaliar.extc.engg@gmail.com>
2024-04-17 23:45:08 -0400

    ReverseNodesInKGroup (neetCode)

--- 6. Linked List/11.ReverseNodesInKGroup.py
+++ 6. Linked List/11.ReverseNodesInKGroup.py
@@ -28,37 +28,3 @@ class Solution:
             curr = curr.next
             k -= 1
         return curr
-
-#vsCode
-
-# Definition for singly-linked list.
-class ListNode:
-    def __init__(self, val=0, next=None):
-        self.val = val
-        self.next = next
-
-class Solution:
-    def reverseKGroup(self, head: ListNode, k: int) -> ListNode:
-        # Manual Input for testing
-        values = list(map(int, input("Enter the values of the linked list nodes separated by space: ").split()))
-        k_input = int(input("Enter the value of k: "))
-
-        # Creating linked list from manual input
-        nodes = [ListNode(val) for val in values]
-        for i in range(len(nodes) - 1):
-            nodes[i].next = nodes[i + 1]
-
-        # Example Usage
-        linked_list_head = nodes[0] if nodes else None
-        solution = Solution()
-        result = solution.reverseKGroup(linked_list_head, k_input)
-
-        # Printing the result
-        while result:
-            print(result.val, end=" ")
-            result = result.next
-        print()
-
-# Example Usage
-solution = Solution()
-solution.reverseKGroup(None, 3)  # You can change the linked list and k for your own testing.
