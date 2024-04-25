a4aac3dfd3592075159d2d545937fa8c6397087b
Rohitm19 <mudaliar.extc.engg@gmail.com>
2024-04-25 18:51:41 -0400

    combinationSum (neetCode)

--- 10. Backtracking/1.combinationSum.py
+++ 10. Backtracking/1.combinationSum.py
@@ -17,38 +17,3 @@ class Solution:
 
         dfs(0, [], 0)
         return res
-
-#vsCode
-
-class Solution:
-    def combinationSum(self, candidates, target):
-        res = []
-
-        def dfs(i, cur, total):
-            if total == target:
-                res.append(cur.copy())
-                return
-            if i >= len(candidates) or total > target:
-                return
-
-            cur.append(candidates[i])
-            dfs(i, cur, total + candidates[i])
-            cur.pop()
-            dfs(i + 1, cur, total)
-
-        candidates = [int(x) for x in input("Enter candidates separated by spaces: ").split()]
-        target = int(input("Enter the target sum: "))
-
-        dfs(0, [], 0)
-        return res
-
-def main():
-    solution = Solution()
-    combinations = solution.combinationSum([], 0)
-    
-    print("Combinations that sum to the target:")
-    for combination in combinations:
-        print(combination)
-
-if __name__ == "__main__":
-    main()
