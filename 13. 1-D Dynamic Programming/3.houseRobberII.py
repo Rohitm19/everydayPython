fc7081a03a64619bfad839325de2e6857b0eb89d
Rohitm19 <mudaliar.extc.engg@gmail.com>
2024-05-03 23:17:01 -0400

    houseRobberII (neetcode)

--- 13. 1-D Dynamic Programming/3.houseRobberII.py
+++ 13. 1-D Dynamic Programming/3.houseRobberII.py
@@ -11,29 +11,3 @@ class Solution:
             rob1 = rob2
             rob2 = newRob
         return rob2
-
-#vsCode
-
-from typing import List
-
-class Solution:
-    def rob(self, nums: List[int]) -> int:
-        return max(nums[0], self.helper(nums[1:]), self.helper(nums[:-1]))
-
-    def helper(self, nums):
-        rob1, rob2 = 0, 0
-
-        for n in nums:
-            newRob = max(rob1 + n, rob2)
-            rob1 = rob2
-            rob2 = newRob
-        return rob2
-
-def main():
-    solution = Solution()
-    nums = [int(x) for x in input("Enter a list of integers for nums, separated by spaces: ").split()]
-    result = solution.rob(nums)
-    print("Maximum amount that can be robbed:", result)
-
-if __name__ == "__main__":
-    main()
