bb633e679bfa05bc2c9ed927d2783f79b786cc1e
Rohitm19 <mudaliar.extc.engg@gmail.com>
2024-05-04 23:20:43 -0400

    longestPalindromicSubstring (neetCode)

--- 13. 1-D Dynamic Programming/6.decodeWays.py
+++ 13. 1-D Dynamic Programming/6.decodeWays.py
@@ -33,30 +33,3 @@ class Solution:
             ):
                 dp[i] += dp[i + 2]
         return dp[0]
-
-#vsCode
-
-class Solution:
-    def numDecodings(self, s: str) -> int:
-        # Dynamic Programming
-        dp = {len(s): 1}
-        for i in range(len(s) - 1, -1, -1):
-            if s[i] == "0":
-                dp[i] = 0
-            else:
-                dp[i] = dp[i + 1]
-
-            if i + 1 < len(s) and (
-                s[i] == "1" or s[i] == "2" and s[i + 1] in "0123456"
-            ):
-                dp[i] += dp[i + 2]
-        return dp[0]
-
-def main():
-    solution = Solution()
-    s = input("Enter the decoding string: ")
-    result = solution.numDecodings(s)
-    print("Number of possible decodings:", result)
-
-if __name__ == "__main__":
-    main()
