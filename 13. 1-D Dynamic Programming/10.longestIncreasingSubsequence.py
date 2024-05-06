72c9f66f33e11a1090352ea53aa85c688ae7955d
Rohitm19 <mudaliar.extc.engg@gmail.com>
2024-05-05 23:18:26 -0400

    longestIncreasingSubsequence (neetCode)

--- 13. 1-D Dynamic Programming/10.longestIncreasingSubsequence.py
+++ 13. 1-D Dynamic Programming/10.longestIncreasingSubsequence.py
@@ -8,24 +8,3 @@ class Solution:
                 if nums[i] < nums[j]:
                     LIS[i] = max(LIS[i], 1 + LIS[j])
         return max(LIS)
-
-#vsCode
-class Solution:
-    def lengthOfLIS(self, nums: List[int]) -> int:
-        LIS = [1] * len(nums)
-
-        for i in range(len(nums) - 1, -1, -1):
-            for j in range(i + 1, len(nums)):
-                if nums[i] < nums[j]:
-                    LIS[i] = max(LIS[i], 1 + LIS[j])
-        return max(LIS)
-
-def main():
-    solution = Solution()
-    nums_input = input("Enter the list of numbers (comma-separated): ")
-    nums = [int(x) for x in nums_input.split(',')]
-    result = solution.lengthOfLIS(nums)
-    print("Length of Longest Increasing Subsequence:", result)
-
-if __name__ == "__main__":
-    main()
