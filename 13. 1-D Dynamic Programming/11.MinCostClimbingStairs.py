fda0a0eb88942f2b299566e9e9bd105c98f1f229
Rohitm19 <mudaliar.extc.engg@gmail.com>
2024-05-05 23:18:51 -0400

    MinCostClimbingStairs (neetCode)

--- 13. 1-D Dynamic Programming/11.MinCostClimbingStairs.py
+++ 13. 1-D Dynamic Programming/11.MinCostClimbingStairs.py
@@ -5,24 +5,3 @@ class Solution:
             cost[i] += min(cost[i + 1], cost[i + 2])
 
         return min(cost[0], cost[1])
-
-#vsCode
-from typing import List
-class Solution:
-    def minCostClimbingStairs(self, cost: List[int]) -> int:
-        for i in range(len(cost) - 3, -1, -1):
-            cost[i] += min(cost[i + 1], cost[i + 2])
-
-        return min(cost[0], cost[1])
-
-# Manual Input
-cost = [10, 15, 20]
-
-# Create an instance of the Solution class
-solution = Solution()
-
-# Call the minCostClimbingStairs method with your input
-min_cost = solution.minCostClimbingStairs(cost)
-
-# Print the result
-print("Minimum Cost:", min_cost)
