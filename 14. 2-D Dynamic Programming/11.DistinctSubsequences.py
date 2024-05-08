cc792b2aca047b0ba4318765f9c3983893b304ae
Rohitm19 <mudaliar.extc.engg@gmail.com>
2024-05-08 14:47:44 -0400

    DistinctSubsequences (neetCode)

--- 14. 2-D Dynamic Programming/11.DistinctSubsequences.py
+++ 14. 2-D Dynamic Programming/11.DistinctSubsequences.py
@@ -15,35 +15,3 @@ class Solution:
                 else:
                     cache[(i, j)] = cache[(i + 1, j)]
         return cache[(0, 0)]
-
-#vsCode
-class Solution:
-    def numDistinct(self, s: str, t: str) -> int:
-        cache = {}
-
-        for i in range(len(s) + 1):
-            cache[(i, len(t))] = 1
-        for j in range(len(t)):
-            cache[(len(s), j)] = 0
-
-        for i in range(len(s) - 1, -1, -1):
-            for j in range(len(t) - 1, -1, -1):
-                if s[i] == t[j]:
-                    cache[(i, j)] = cache[(i + 1, j + 1)] + cache[(i + 1, j)]
-                else:
-                    cache[(i, j)] = cache[(i + 1, j)]
-
-        return cache[(0, 0)]
-
-# Manual Input
-s = "rabbbit"  # Replace this with your input
-t = "rabbit"   # Replace this with your input
-
-# Create an instance of the Solution class
-solution = Solution()
-
-# Call the numDistinct method with your input
-result = solution.numDistinct(s, t)
-
-# Print the result
-print("Number of Distinct Subsequences:", result)
