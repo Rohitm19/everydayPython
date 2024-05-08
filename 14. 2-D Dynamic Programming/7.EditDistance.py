e9a377d284c6842be99bfce26e247a1ed2132838
Rohitm19 <mudaliar.extc.engg@gmail.com>
2024-05-07 23:31:49 -0400

    EditDistance (neetCode)

--- 14. 2-D Dynamic Programming/7.EditDistance.py
+++ 14. 2-D Dynamic Programming/7.EditDistance.py
@@ -15,34 +15,3 @@ class Solution:
                 else:
                     dp[i][j] = 1 + min(dp[i + 1][j], dp[i][j + 1], dp[i + 1][j + 1])
         return dp[0][0]
-
-#vsCode
-class Solution:
-    def minDistance(self, word1: str, word2: str) -> int:
-        dp = [[float("inf")] * (len(word2) + 1) for i in range(len(word1) + 1)]
-
-        for j in range(len(word2) + 1):
-            dp[len(word1)][j] = len(word2) - j
-        for i in range(len(word1) + 1):
-            dp[i][len(word2)] = len(word1) - i
-
-        for i in range(len(word1) - 1, -1, -1):
-            for j in range(len(word2) - 1, -1, -1):
-                if word1[i] == word2[j]:
-                    dp[i][j] = dp[i + 1][j + 1]
-                else:
-                    dp[i][j] = 1 + min(dp[i + 1][j], dp[i][j + 1], dp[i + 1][j + 1])
-        return dp[0][0]
-
-# Manual Input
-word1 = "horse"  # Replace this with your input
-word2 = "ros"    # Replace this with your input
-
-# Create an instance of the Solution class
-solution = Solution()
-
-# Call the minDistance method with your input
-result = solution.minDistance(word1, word2)
-
-# Print the result
-print("Minimum Distance:", result)
