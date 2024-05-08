b063b2316c6bebd9d1b9cad176f85eebfd38e12b
Rohitm19 <mudaliar.extc.engg@gmail.com>
2024-05-07 23:31:07 -0400

    InterleavingString (neetCode)

--- 14. 2-D Dynamic Programming/6.InterleavingString.py
+++ 14. 2-D Dynamic Programming/6.InterleavingString.py
@@ -14,34 +14,3 @@ class Solution:
                 if j < len(s2) and s2[j] == s3[i + j] and dp[i][j + 1]:
                     dp[i][j] = True
         return dp[0][0]
-
-#vsCode
-class Solution:
-    def isInterleave(self, s1: str, s2: str, s3: str) -> bool:
-        if len(s1) + len(s2) != len(s3):
-            return False
-
-        dp = [[False] * (len(s2) + 1) for i in range(len(s1) + 1)]
-        dp[len(s1)][len(s2)] = True
-
-        for i in range(len(s1), -1, -1):
-            for j in range(len(s2), -1, -1):
-                if i < len(s1) and s1[i] == s3[i + j] and dp[i + 1][j]:
-                    dp[i][j] = True
-                if j < len(s2) and s2[j] == s3[i + j] and dp[i][j + 1]:
-                    dp[i][j] = True
-        return dp[0][0]
-
-# Manual Input
-s1 = "aabcc"  # Replace this with your input
-s2 = "dbbca"  # Replace this with your input
-s3 = "aadbbcbcac"  # Replace this with your input
-
-# Create an instance of the Solution class
-solution = Solution()
-
-# Call the isInterleave method with your input
-result = solution.isInterleave(s1, s2, s3)
-
-# Print the result
-print("Is it possible to interleave the strings? ", result)
