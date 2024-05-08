04011644309a4becb8decc81a66acf0390604b4c
Rohitm19 <mudaliar.extc.engg@gmail.com>
2024-05-07 23:30:43 -0400

    TargetSum (neetCode)

--- 14. 2-D Dynamic Programming/5.TargetSum.py
+++ 14. 2-D Dynamic Programming/5.TargetSum.py
@@ -15,36 +15,3 @@ class Solution:
             return dp[(i, total)]
 
         return backtrack(0, 0)
-
-#vsCode
-from typing import List
-
-class Solution:
-    def findTargetSumWays(self, nums: List[int], target: int) -> int:
-        dp = {}  # (index, total) -> # of ways
-
-        def backtrack(i, total):
-            if i == len(nums):
-                return 1 if total == target else 0
-            if (i, total) in dp:
-                return dp[(i, total)]
-
-            dp[(i, total)] = backtrack(i + 1, total + nums[i]) + backtrack(
-                i + 1, total - nums[i]
-            )
-            return dp[(i, total)]
-
-        return backtrack(0, 0)
-
-# Manual Input
-nums = [1, 1, 1, 1, 1]  # Replace this with your input
-target = 3  # Replace this with your input
-
-# Create an instance of the Solution class
-solution = Solution()
-
-# Call the findTargetSumWays method with your input
-result = solution.findTargetSumWays(nums, target)
-
-# Print the result
-print("The number of ways to reach the target is:", result)
