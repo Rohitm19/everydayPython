2a1f5487179ee5a12c700bdec086e6e6df180a22
Rohitm19 <mudaliar.extc.engg@gmail.com>
2024-05-09 23:31:57 -0400

    JumpGameII (neetCode)

--- 15. Greedy/3.JumpGameII.py
+++ 15. Greedy/3.JumpGameII.py
@@ -11,30 +11,3 @@ class Solution:
             r = maxJump
             res += 1
         return res
-
-#vsCode
-
-class Solution:
-    def jump(self, nums: List[int]) -> int:
-        l, r = 0, 0
-        res = 0
-        while r < (len(nums) - 1):
-            maxJump = 0
-            for i in range(l, r + 1):
-                maxJump = max(maxJump, i + nums[i])
-            l = r + 1
-            r = maxJump
-            res += 1
-        return res
-
-# Manual Input
-nums = [2, 3, 1, 1, 4]  # Replace this with your input
-
-# Create an instance of the Solution class
-solution = Solution()
-
-# Call the jump method with your input
-result = solution.jump(nums)
-
-# Print the result
-print("Minimum number of jumps:", result)
