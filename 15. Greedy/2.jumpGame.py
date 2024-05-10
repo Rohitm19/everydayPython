3a56f3136da367b86c3ef218d0c8390d69dd2476
Rohitm19 <mudaliar.extc.engg@gmail.com>
2024-05-09 23:31:35 -0400

    jumpGame (neetCode)

--- 15. Greedy/2.jumpGame.py
+++ 15. Greedy/2.jumpGame.py
@@ -7,25 +7,3 @@ class Solution:
             if i + nums[i] >= goal:
                 goal = i
         return goal == 0
-
-#vsCode
-
-from typing import List
-
-class Solution:
-    def canJump(self, nums: List[int]) -> bool:
-        goal = len(nums) - 1
-
-        for i in range(len(nums) - 2, -1, -1):
-            if i + nums[i] >= goal:
-                goal = i
-        return goal == 0
-
-def main():
-    solution = Solution()
-    nums = list(map(int, input("Enter the list of numbers, separated by spaces: ").split()))
-    result = solution.canJump(nums)
-    print("Can jump to the last index:", result)
-
-if __name__ == "__main__":
-    main()
