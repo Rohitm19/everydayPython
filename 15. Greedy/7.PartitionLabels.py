76c9051958af3881a76f07fe281c2e7f99545a06
Rohitm19 <mudaliar.extc.engg@gmail.com>
2024-05-11 23:11:43 -0400

    PartitionLabels (neetCode)

--- 15. Greedy/7.PartitionLabels.py
+++ 15. Greedy/7.PartitionLabels.py
@@ -20,34 +20,3 @@ class Solution:
                 curLen = 0
             i += 1
         return res
-
-#vsCode
-class Solution:
-    def partitionLabels(self, S: str) -> List[int]:
-        count = {}
-        res = []
-        i, length = 0, len(S)
-        for j in range(length):
-            c = S[j]
-            count[c] = j
-
-        curLen = 0
-        goal = 0
-        while i < length:
-            c = S[i]
-            goal = max(goal, count[c])
-            curLen += 1
-
-            if goal == i:
-                res.append(curLen)
-                curLen = 0
-            i += 1
-        return res
-
-# Manual Input
-input_string = "abacab"
-solution = Solution()
-result = solution.partitionLabels(input_string)
-
-# Print the result
-print("Partition Labels:", result)
