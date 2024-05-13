8310778360480fd166f7b74d5be83be15ec9afe7
Rohitm19 <mudaliar.extc.engg@gmail.com>
2024-05-12 22:48:59 -0400

    meetingRoomsII (neetCode)

--- 16.Intervals/5.meetingRoomsII.py
+++ 16.Intervals/5.meetingRoomsII.py
@@ -13,36 +13,3 @@ def minMeetingRooms(self, intervals: List[List[int]]) -> int:
             count += t[1]
             max_count = max(max_count, count)
         return max_count
-
-#vsCode
-from typing import List
-
-class Solution:
-    def minMeetingRooms(self, intervals: List[List[int]]) -> int:
-        time = []
-        for i in intervals:
-            time.append((i[0], 1))
-            time.append((i[1], -1))
-
-        time.sort(key=lambda x: (x[0], x[1]))
-
-        count = 0
-        max_count = 0
-        for t in time:
-            count += t[1]
-            max_count = max(max_count, count)
-        return max_count
-
-def main():
-    solution = Solution()
-    intervals = []
-    num_intervals = int(input("Enter the number of intervals: "))
-    for i in range(num_intervals):
-        interval = list(map(int, input(f"Enter interval {i + 1} as [start, end]: ").split(',')))
-        intervals.append(interval)
-
-    result = solution.minMeetingRooms(intervals)
-    print("Minimum meeting rooms required:", result)
-
-if __name__ == "__main__":
-    main()
