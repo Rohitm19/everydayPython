b8abbe45e2c63121d5c107eae3ada1839ce16ab1
Rohitm19 <mudaliar.extc.engg@gmail.com>
2024-05-13 23:35:05 -0400

    MinimumIntervaltoIncludeEachQuery (neetCode)

--- 16.Intervals/6.MinimumIntervaltoIncludeEachQuery.py
+++ 16.Intervals/6.MinimumIntervaltoIncludeEachQuery.py
@@ -15,34 +15,3 @@ class Solution:
                 heapq.heappop(minHeap)
             res[q] = minHeap[0][0] if minHeap else -1
         return [res[q] for q in queries]
-
-#vsCode
-
-from typing import List
-import heapq
-
-class Solution:
-    def minInterval(self, intervals: List[List[int]], queries: List[int]) -> List[int]:
-        intervals.sort()
-        minHeap = []
-        res = {}
-        i = 0
-        for q in sorted(queries):
-            while i < len(intervals) and intervals[i][0] <= q:
-                l, r = intervals[i]
-                heapq.heappush(minHeap, (r - l + 1, r))
-                i += 1
-
-            while minHeap and minHeap[0][1] < q:
-                heapq.heappop(minHeap)
-            res[q] = minHeap[0][0] if minHeap else -1
-        return [res[q] for q in queries]
-
-# Manual Input
-intervals_input = [[1, 4], [2, 4], [3, 6]]
-queries_input = [2, 3, 4, 5]
-solution = Solution()
-
-# Using the method
-result = solution.minInterval(intervals_input, queries_input)
-print("Result:", result)
