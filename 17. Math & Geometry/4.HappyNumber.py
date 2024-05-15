9b62924b6aabad75f6542482a633408b2581b686
Rohitm19 <mudaliar.extc.engg@gmail.com>
2024-05-14 22:43:37 -0400

    HappyNumber (neetCode)

--- 17. Math & Geometry/4.HappyNumber.py
+++ 17. Math & Geometry/4.HappyNumber.py
@@ -1,31 +1,3 @@
-#vsCode
-
-class Solution:
-    def isHappy(self, n: int) -> bool:
-        slow, fast = n, self.sumSquareDigits(n)
-
-        while slow != fast:
-            fast = self.sumSquareDigits(fast)
-            fast = self.sumSquareDigits(fast)
-            slow = self.sumSquareDigits(slow)
-
-        return True if fast == 1 else False
-
-    def sumSquareDigits(self, n):
-        output = 0
-        while n:
-            output += (n % 10) ** 2
-            n = n // 10
-        return output
-
-# Manual Input
-input_number = 19
-solution = Solution()
-
-# Using the method
-result = solution.isHappy(input_number)
-print("Result:", result)
-
 #neetCode
 class Solution:
     def isHappy(self, n: int) -> bool:
