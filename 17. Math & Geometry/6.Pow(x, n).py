e076803b06073e0350840c3e1b021a90a10f0604
Rohitm19 <mudaliar.extc.engg@gmail.com>
2024-05-14 22:44:18 -0400

    Pow(x, n) (neetCode)

--- 17. Math & Geometry/6.Pow(x, n).py
+++ 17. Math & Geometry/6.Pow(x, n).py
@@ -12,29 +12,3 @@ class Solution:
 
         res = helper(x, abs(n))
         return res if n >= 0 else 1 / res
-
-#vsCode
-
-class Solution:
-    def myPow(self, x: float, n: int) -> float:
-        def helper(x, n):
-            if x == 0:
-                return 0
-            if n == 0:
-                return 1
-
-            res = helper(x * x, n // 2)
-            return x * res if n % 2 else res
-
-        # Manual Input
-        base = float(input("Enter the base (x): "))
-        exponent = int(input("Enter the exponent (n): "))
-
-        result = self.myPow(base, exponent)
-        print("Result:", result)
-
-# Create an instance of the Solution class
-solution = Solution()
-
-# Use the myPow method with manual input
-solution.myPow(x, n)
