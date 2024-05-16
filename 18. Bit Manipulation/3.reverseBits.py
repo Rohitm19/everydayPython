3e8454db6cff2a5f595ef4dde9481fc2d732d091
Rohitm19 <mudaliar.extc.engg@gmail.com>
2024-05-15 22:45:51 -0400

    reverseBits (neetCode)

--- 18. Bit Manipulation/3.reverseBits.py
+++ 18. Bit Manipulation/3.reverseBits.py
@@ -6,22 +6,3 @@ class Solution:
             bit = (n >> i) & 1
             res += (bit << (31 - i))
         return res
-
-#vsCode
-
-class Solution:
-    def reverseBits(self, n: int) -> int:
-        res = 0
-        for i in range(32):
-            bit = (n >> i) & 1
-            res += (bit << (31 - i))
-        return res
-
-def main():
-    solution = Solution()
-    n = int(input("Enter the value of n: "))
-    result = solution.reverseBits(n)
-    print("Reversed bits of", n, "is", result)
-
-if __name__ == "__main__":
-    main()
