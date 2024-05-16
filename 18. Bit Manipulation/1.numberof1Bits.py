450f4cf4085d929a16e988fe82e43be15f7bec84
Rohitm19 <mudaliar.extc.engg@gmail.com>
2024-05-15 22:44:09 -0400

    numberof1Bits (neetCode)

--- 18. Bit Manipulation/1.numberof1Bits.py
+++ 18. Bit Manipulation/1.numberof1Bits.py
@@ -6,22 +6,3 @@ class Solution:
             n &= n - 1
             res += 1
         return res
-
-#vsCode
-
-class Solution:
-    def hammingWeight(self, n: int) -> int:
-        res = 0
-        while n:
-            n &= n - 1
-            res += 1
-        return res
-
-def main():
-    solution = Solution()
-    n = int(input("Enter the value of n: "))
-    result = solution.hammingWeight(n)
-    print("Hamming weight of", n, "is", result)
-
-if __name__ == "__main__":
-    main()
