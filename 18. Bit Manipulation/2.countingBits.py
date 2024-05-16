20d9516b5655025a57d48ef5d37ef7160c8b7dca
Rohitm19 <mudaliar.extc.engg@gmail.com>
2024-05-15 22:44:31 -0400

    countingBits (neetCode)

--- 18. Bit Manipulation/2.countingBits.py
+++ 18. Bit Manipulation/2.countingBits.py
@@ -9,25 +9,3 @@ class Solution:
                 offset = i
             dp[i] = 1 + dp[i - offset]
         return dp
-
-#vsCode
-
-class Solution:
-    def countBits(self, n: int) -> List[int]:
-        dp = [0] * (n + 1)
-        offset = 1
-
-        for i in range(1, n + 1):
-            if offset * 2 == i:
-                offset = i
-            dp[i] = 1 + dp[i - offset]
-        return dp
-
-def main():
-    solution = Solution()
-    n = int(input("Enter the value of n: "))
-    result = solution.countBits(n)
-    print("Bit count for each number up to", n, "is", result)
-
-if __name__ == "__main__":
-    main()
