8c97e7a1c2d14ddef34b6f6973959aa6b6e49350
Rohitm19 <mudaliar.extc.engg@gmail.com>
2024-05-16 22:11:07 -0400

    SingleNumber (neetCode)

--- 18. Bit Manipulation/6.SingleNumber.py
+++ 18. Bit Manipulation/6.SingleNumber.py
@@ -5,22 +5,3 @@ class Solution:
         for n in nums:
             res = n ^ res
         return res
-
-#vsCode
-
-from typing import List
-
-class Solution:
-    def singleNumber(self, nums: List[int]) -> int:
-        # Manual Input for testing
-        nums_input = list(map(int, input("Enter space-separated numbers: ").split()))
-
-        res = 0
-        for n in nums_input:
-            res = n ^ res
-        return res
-
-# Example Usage
-solution = Solution()
-result = solution.singleNumber([])
-print(f"Single Number: {result}")
