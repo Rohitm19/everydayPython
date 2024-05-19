ca5cce7fb529b21fa2767c5c0cc2f2cee2b5855a
Rohitm19 <mudaliar.extc.engg@gmail.com>
2024-05-18 23:45:16 -0400

    productExceptSelf (neetCode)

--- 1. Arrays & Hashing/6.productExceptSelf.py
+++ 1. Arrays & Hashing/6.productExceptSelf.py
@@ -2,7 +2,6 @@
 class Solution:
     def productExceptSelf(self, nums: List[int]) -> List[int]:
         res = [1] * (len(nums))
-
         for i in range(1, len(nums)):
             res[i] = res[i-1] * nums[i-1]
         postfix = 1
