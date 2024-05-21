699986634dd1ae3eab7c202c441bbc6d0be79db8
Rohitm19 <mudaliar.extc.engg@gmail.com>
2024-05-20 22:55:41 -0400

    TwoSumIIInputArrayIsSorted (neetCode)

--- 2. Two Pointers/4.TwoSumIIInputArrayIsSorted.py
+++ 2. Two Pointers/4.TwoSumIIInputArrayIsSorted.py
@@ -2,7 +2,6 @@
 class Solution:
     def twoSum(self, numbers: List[int], target: int) -> List[int]:
         l, r = 0, len(numbers) - 1
-
         while l < r:
             curSum = numbers[l] + numbers[r]
 
