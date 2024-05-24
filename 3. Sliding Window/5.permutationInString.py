4c819dcb18dfdaea73d21b2b721a7f014ba5077b
Rohitm19 <mudaliar.extc.engg@gmail.com>
2024-05-23 23:53:03 -0400

    permutationInString (neetCode)

--- 3. Sliding Window/5.permutationInString.py
+++ 3. Sliding Window/5.permutationInString.py
@@ -3,7 +3,6 @@ class Solution:
     def checkInclusion(self, s1: str, s2: str) -> bool:
         if len(s1) > len(s2):
             return False
-
         s1Count, s2Count = [0] * 26, [0] * 26
         for i in range(len(s1)):
             s1Count[ord(s1[i]) - ord("a")] += 1
