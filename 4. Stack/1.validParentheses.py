a764480d4b74ce6815a569e821a222d5d744c9f0
Rohitm19 <mudaliar.extc.engg@gmail.com>
2024-05-26 22:34:49 -0400

    va;

--- 4. Stack/1.validParentheses.py
+++ 4. Stack/1.validParentheses.py
@@ -3,7 +3,6 @@ class Solution:
     def isValid(self, s: str) -> bool:
         Map = {")": "(", "]": "[", "}": "{"}
         stack = []
-
         for c in s:
             if c not in Map:
                 stack.append(c)
