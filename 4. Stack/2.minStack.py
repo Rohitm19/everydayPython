18bbf6eb8dc69454572ad397377c5f01a4e5b7a2
Rohitm19 <mudaliar.extc.engg@gmail.com>
2024-05-26 22:35:15 -0400

    minStack (neetCode)

--- 4. Stack/2.minStack.py
+++ 4. Stack/2.minStack.py
@@ -3,7 +3,6 @@ class MinStack:
     def __init__(self):
         self.stack = []
         self.minStack = []
-
     def push(self, val: int) -> None:
         self.stack.append(val)
         val = min(val, self.minStack[-1] if self.minStack else val)
