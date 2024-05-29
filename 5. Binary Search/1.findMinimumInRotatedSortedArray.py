cf9dc1f017d57c707d8254baa8134884ea54bd3e
Rohitm19 <mudaliar.extc.engg@gmail.com>
2024-05-28 23:50:42 -0400

    findMinimumInRotatedSortedArray (neetCode)

--- 5. Binary Search/1.findMinimumInRotatedSortedArray.py
+++ 5. Binary Search/1.findMinimumInRotatedSortedArray.py
@@ -3,7 +3,6 @@ class Solution:
     def findMin(self, nums: List[int]) -> int:
         start , end = 0, len(nums) - 1 
         curr_min = float("inf")
-        
         while start  <  end :
             mid = (start + end ) // 2
             curr_min = min(curr_min,nums[mid])
