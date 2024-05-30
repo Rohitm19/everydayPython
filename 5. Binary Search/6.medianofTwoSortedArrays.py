b2e99f3ccdae3c8e62344086de0afb9bf4eebe40
Rohitm19 <mudaliar.extc.engg@gmail.com>
2024-05-29 23:46:30 -0400

    medianofTwoSortedArrays (neetCode)

--- 5. Binary Search/6.medianofTwoSortedArrays.py
+++ 5. Binary Search/6.medianofTwoSortedArrays.py
@@ -6,8 +6,7 @@ class Solution:
     def findMedianSortedArrays(self, nums1: List[int], nums2: List[int]) -> float:
         A, B = nums1, nums2
         total = len(nums1) + len(nums2)
-        half = total // 2
-
+        half = total 
         if len(B) < len(A):
             A, B = B, A
 
