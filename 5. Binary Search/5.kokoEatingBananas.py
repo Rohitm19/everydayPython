123ba85c99c6fa5468306c1301db7a52694a08e7
Rohitm19 <mudaliar.extc.engg@gmail.com>
2024-05-29 23:46:10 -0400

    kokoEatingBananas (neetCode)

--- 5. Binary Search/5.kokoEatingBananas.py
+++ 5. Binary Search/5.kokoEatingBananas.py
@@ -3,7 +3,6 @@ class Solution:
     def minEatingSpeed(self, piles: List[int], h: int) -> int:
         l, r = 1, max(piles)
         res = r
-
         while l <= r:
             k = (l + r) // 2
 
