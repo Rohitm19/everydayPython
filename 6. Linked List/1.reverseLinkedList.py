d69320117c99c745923f67ec218e0285063b8504
Rohitm19 <mudaliar.extc.engg@gmail.com>
2024-05-29 23:47:15 -0400

    reverseLinkedList (neetCode)

--- 6. Linked List/1.reverseLinkedList.py
+++ 6. Linked List/1.reverseLinkedList.py
@@ -1,11 +1,3 @@
-#neetCode
-# Definition for singly-linked list.
-# class ListNode:
-#     def __init__(self, x):
-#         self.val = x
-#         self.next = None
-
-
 class Solution:
     def reverseList(self, head: ListNode) -> ListNode:
         prev, curr = None, head
