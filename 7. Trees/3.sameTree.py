2ce515523ed6d9635f67b66a74aeb35bd6c4877e
Rohitm19 <mudaliar.extc.engg@gmail.com>
2024-06-05 23:30:44 -0400

    sameTree (neetCode)

--- 7. Trees/3.sameTree.py
+++ 7. Trees/3.sameTree.py
@@ -1,12 +1,4 @@
 #neetCode
-# Definition for a binary tree node.
-# class TreeNode:
-#     def __init__(self, x):
-#         self.val = x
-#         self.left = None
-#         self.right = None
-
-
 class Solution:
     def isSameTree(self, p: TreeNode, q: TreeNode) -> bool:
         if not p and not q:
