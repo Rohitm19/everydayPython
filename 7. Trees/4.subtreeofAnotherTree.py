4a9b83bd553be2a05383b8b1dbd71db0c8c3b86b
Rohitm19 <mudaliar.extc.engg@gmail.com>
2024-06-05 23:31:10 -0400

    subtreeofAnotherTree (neetCode)

--- 7. Trees/4.subtreeofAnotherTree.py
+++ 7. Trees/4.subtreeofAnotherTree.py
@@ -1,10 +1,4 @@
 #neetCode
-# Definition for a binary tree node.
-# class TreeNode:
-#     def __init__(self, val=0, left=None, right=None):
-#         self.val = val
-#         self.left = left
-#         self.right = right
 class Solution:
     def isSubtree(self, s: TreeNode, t: TreeNode) -> bool:
         if not t:
@@ -15,7 +9,6 @@ class Solution:
         if self.sameTree(s, t):
             return True
         return self.isSubtree(s.left, t) or self.isSubtree(s.right, t)
-
     def sameTree(self, s, t):
         if not s and not t:
             return True
