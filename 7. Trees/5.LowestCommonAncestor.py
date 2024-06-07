21d5f76c533f4033cfd9fbe602373a6185a11869
Rohitm19 <mudaliar.extc.engg@gmail.com>
2024-06-06 22:07:02 -0400

    LowestCommonAncestor (neetCode)

--- 7. Trees/5.LowestCommonAncestor.py
+++ 7. Trees/5.LowestCommonAncestor.py
@@ -1,12 +1,4 @@
 #neetCode
-# Definition for a binary tree node.
-# class TreeNode:
-#     def __init__(self, x):
-#         self.val = x
-#         self.left = None
-#         self.right = None
-
-
 class Solution:
     def lowestCommonAncestor(
         self, root: "TreeNode", p: "TreeNode", q: "TreeNode"
