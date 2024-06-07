7d975ea584a77c654f7c17ae5b64c5e2b7f5f480
Rohitm19 <mudaliar.extc.engg@gmail.com>
2024-06-06 22:08:28 -0400

    KthSmallestElementInaBst (neetCode)

--- 7. Trees/8.KthSmallestElementInaBst.py
+++ 7. Trees/8.KthSmallestElementInaBst.py
@@ -1,11 +1,4 @@
 #neetcode
-# Definition for a binary tree node.
-# class TreeNode:
-#     def __init__(self, x):
-#         self.val = x
-#         self.left = None
-#         self.right = None
-
 
 class Solution:
     def kthSmallest(self, root: TreeNode, k: int) -> int:
