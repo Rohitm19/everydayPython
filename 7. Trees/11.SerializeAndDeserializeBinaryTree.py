f49faae4f671165e081068ac558352fdaaa8eee4
Rohitm19 <mudaliar.extc.engg@gmail.com>
2024-06-10 23:14:46 -0400

    SerializeAndDeserializeBinaryTree (neetCode)

--- 7. Trees/11.SerializeAndDeserializeBinaryTree.py
+++ 7. Trees/11.SerializeAndDeserializeBinaryTree.py
@@ -1,12 +1,4 @@
 #neetCode
-# Definition for a binary tree node.
-# class TreeNode(object):
-#     def __init__(self, x):
-#         self.val = x
-#         self.left = None
-#         self.right = None
-
-
 class Codec:
     def serialize(self, root):
         res = []
